ceb16a3c37babaf238bc59863758d9720c81bcb9
rito528 <rotoyutoriapp@gmail.com>
2021-09-17 22:11:43 +0900

    レベル関係のコマンドを追加

--- src/main/scala/com/ryoserver/Level/LevelCommand.scala
+++ src/main/scala/com/ryoserver/Level/LevelCommand.scala
@@ -1,14 +1,31 @@
 package com.ryoserver.Level
 
+import org.bukkit.ChatColor
 import org.bukkit.command.{Command, CommandExecutor, CommandSender}
 
 class LevelCommand extends CommandExecutor {
 
   override def onCommand(sender: CommandSender, command: Command, label: String, args: Array[String]): Boolean = {
     if (label.equalsIgnoreCase("level")) {
-      if (args(0).equalsIgnoreCase("test")) {
-        val lv = new CalLv()
-        println("Lv." + lv.getLevel(141983))
+      val lv = new CalLv()
+      if (args(0).equalsIgnoreCase("getLevel")) {
+        sender.sendMessage(args(1) + "に相当するレベル" + lv.getLevel(args(1).toInt))
+        return true
+      } else if (args(0).equalsIgnoreCase("getExp")) {
+        sender.sendMessage("Lv." + args(1) + "->Lv." + (args(1).toInt + 1) + "までに必要な経験値量:" + lv.getExp(args(1).toInt + 1))
+        return true
+      } else if (args(0).equalsIgnoreCase("getTotalExp")) {
+        sender.sendMessage("Lv." + args(1) + "までに必要な経験値総量:" + lv.getSumTotal(args(1).toInt))
+        return true
+      } else if (args(0).equalsIgnoreCase("help")) {
+        sender.sendMessage("+-------------------------------------+")
+        sender.sendMessage(ChatColor.AQUA + "/level getLevel <経験値>")
+        sender.sendMessage("指定した経験値に相当するレベルを表示します。")
+        sender.sendMessage(ChatColor.AQUA + "/level getExp <レベル>")
+        sender.sendMessage("指定したレベルにから次のレベルに到達するまでに必要な経験値量を取得します。")
+        sender.sendMessage(ChatColor.AQUA + "/level getTotalExp <レベル>")
+        sender.sendMessage("指定したレベルに到達するまでに必要な経験値総量を表示します。")
+        sender.sendMessage("+-------------------------------------+")
         return true
       }
     }
