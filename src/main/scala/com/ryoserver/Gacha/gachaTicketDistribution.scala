fd0d7ee9f0cdec2d9d942920a81b766f894c1c15
rito528 <rotoyutoriapp@gmail.com>
2021-09-20 20:29:06 +0900

    レベルアップ報酬、経験値報酬でガチャ券をもらえるように

---
+++ src/main/scala/com/ryoserver/Gacha/gachaTicketDistribution.scala
@@ -0,0 +1,9 @@
+package com.ryoserver.Gacha
+
+class gachaTicketDistribution {
+
+  def Distribution(nowExp:Int,addedExp:Int,level:Int): Unit = {
+
+  }
+
+}
--- src/main/scala/com/ryoserver/Level/Player/updateLevel.scala
+++ src/main/scala/com/ryoserver/Level/Player/updateLevel.scala
@@ -9,7 +9,24 @@ class updateLevel(ryoServerAssist: RyoServerAssist) {
 
   def updateExp(exp: Int,p:Player): Unit = {
     val sql = new SQL(ryoServerAssist)
-    sql.executeSQL(s"UPDATE Players SET EXP=$exp,Level=${new CalLv(ryoServerAssist).getLevel(exp)} WHERE UUID='${p.getUniqueId.toString}';")
+    val calLv = new CalLv(ryoServerAssist)
+    val data = sql.executeQuery(s"SELECT EXP,Level FROM Players WHERE UUID='${p.getUniqueId.toString}'")
+    var old_exp = 0
+    var old_level = 0
+    if (data.next()) {
+      old_exp = data.getInt("EXP")
+      old_level = data.getInt("Level")
+    }
+    val sumExp = exp + old_exp
+    val ticketAmountCal = (sumExp / 100) - (old_exp / 100)
+    if ((ticketAmountCal / 100) <= 1) {
+      sql.executeSQL(s"UPDATE Players SET gachaTickets=gachaTickets + $ticketAmountCal WHERE UUID='${p.getUniqueId.toString}';")
+    }
+    if (calLv.getLevel(exp) != old_level && calLv.getLevel(exp) > old_exp) {
+      val ticketCal = (calLv.getLevel(exp) - old_level) * 32
+      sql.executeSQL(s"UPDATE Players SET gachaTickets=gachaTickets + $ticketCal WHERE UUID='${p.getUniqueId.toString}'")
+    }
+    sql.executeSQL(s"UPDATE Players SET EXP=$exp,Level=${calLv.getLevel(exp)} WHERE UUID='${p.getUniqueId.toString}';")
     sql.close()
     BossBar.updateLevelBer(ryoServerAssist,exp, p)
   }
