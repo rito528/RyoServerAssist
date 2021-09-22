c9248a3faf4875ff295f9bb54d1e4563e595966e
rito528 <rotoyutoriapp@gmail.com>
2021-09-22 20:22:44 +0900

    skillコマンドの一部改善

--- src/main/scala/com/ryoserver/SkillSystems/SkillCommands.scala
+++ src/main/scala/com/ryoserver/SkillSystems/SkillCommands.scala
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player
 class SkillCommands extends CommandExecutor {
 
   override def onCommand(sender: CommandSender, command: Command, label: String, args: Array[String]): Boolean = {
+    if (args.length != 2) return false
     if (label.equalsIgnoreCase("skill")) {
       if (args(0).equalsIgnoreCase("getItem")) {
         if (args(1).equalsIgnoreCase("min")) {
