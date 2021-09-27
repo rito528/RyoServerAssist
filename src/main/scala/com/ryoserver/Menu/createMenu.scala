2c18c788ab8423ee3742f8d2dfd82f6e3b009598
rito528 <rotoyutoriapp@gmail.com>
2021-09-27 20:00:41 +0900

    メニュー画面文字色の改善

--- src/main/scala/com/ryoserver/Menu/createMenu.scala
+++ src/main/scala/com/ryoserver/Menu/createMenu.scala
@@ -1,13 +1,13 @@
 package com.ryoserver.Menu
 
-import com.ryoserver.Gacha.GachaPaperData
 import com.ryoserver.Inventory.Item.{getGachaItem, getItem}
 import com.ryoserver.Level.Player.getPlayerData
 import com.ryoserver.Player.getData
 import com.ryoserver.RyoServerAssist
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
-import org.bukkit.{Bukkit, Material}
+import org.bukkit.{Bukkit, ChatColor, Material}
+import org.bukkit.ChatColor._
 
 import scala.jdk.CollectionConverters._
 
@@ -15,17 +15,17 @@ object createMenu {
 
   def menu(p:Player,ryoServerAssist: RyoServerAssist): Inventory = {
     val inv = Bukkit.createInventory(null,27,"りょう鯖メニュー")
-    inv.setItem(0,getItem(Material.SHULKER_BOX,"運営からのガチャ券を受け取ります。",List("クリックで受け取ります。").asJava))
-    inv.setItem(2,getItem(Material.CHEST,"ストレージを開きます。",List("クリックで開きます。").asJava))
-    inv.setItem(4,getItem(Material.BOOK,"クエストを選択します。",List("クリックで開きます。").asJava))
-    inv.setItem(6,getGachaItem(Material.PAPER,"ガチャ券を受け取ります。",List("クリックで受け取ります。",
-      "ガチャ券はEXPが100毎に1枚、または",
-      "レベルが10上がる毎に32枚手に入ります。",
-      "受け取れるガチャ券の枚数:" + new getData(ryoServerAssist).getTickets(p) + "枚",
-      "次のガチャ券まであと" + (100 - new getPlayerData(ryoServerAssist).getPlayerExp(p) % 100)).asJava))
-    inv.setItem(8,getItem(Material.BEACON,"スキルを選択します。",List("クリックで開きます。").asJava))
-    inv.setItem(9,getItem(Material.WOODEN_AXE,"保護メニューを開きます。",List("クリックで開きます。").asJava))
-    inv.setItem(11,getItem(Material.LAVA_BUCKET,"ゴミ箱を開きます。",List("クリックで開きます。","取扱注意！").asJava))
+    inv.setItem(0,getItem(Material.SHULKER_BOX,s"${GREEN}運営からのガチャ券を受け取ります。",List(s"${GRAY}クリックで受け取ります。").asJava))
+    inv.setItem(2,getItem(Material.CHEST,s"${GREEN}ストレージを開きます。",List(s"${GRAY}クリックで開きます。").asJava))
+    inv.setItem(4,getItem(Material.BOOK,s"${GREEN}クエストを選択します。",List(s"${GRAY}クリックで開きます。").asJava))
+    inv.setItem(6,getGachaItem(Material.PAPER,s"${GREEN}ガチャ券を受け取ります。",List(s"${GRAY}クリックで受け取ります。",
+      s"${GRAY}ガチャ券はEXPが100毎に1枚、または",
+      s"${GRAY}レベルが10上がる毎に32枚手に入ります。",
+      s"${GRAY}受け取れるガチャ券の枚数:" + new getData(ryoServerAssist).getTickets(p) + "枚",
+      s"${GRAY}次のガチャ券まであと" + (100 - new getPlayerData(ryoServerAssist).getPlayerExp(p) % 100)).asJava))
+    inv.setItem(8,getItem(Material.BEACON,s"${GREEN}スキルを選択します。",List(s"${GRAY}クリックで開きます。").asJava))
+    inv.setItem(9,getItem(Material.WOODEN_AXE,s"${GREEN}保護メニューを開きます。",List(s"${GRAY}クリックで開きます。").asJava))
+    inv.setItem(11,getItem(Material.LAVA_BUCKET,s"${GREEN}ゴミ箱を開きます。",List(s"${GRAY}クリックで開きます。",s"${GRAY}取扱注意！").asJava))
     inv
   }
 }
--- src/main/scala/com/ryoserver/SkillSystems/Skill/SkillToggle.scala
+++ src/main/scala/com/ryoserver/SkillSystems/Skill/SkillToggle.scala
@@ -4,7 +4,7 @@ import com.ryoserver.RyoServerAssist
 import com.ryoserver.SkillSystems.Skill.PlayerSkillData.enableSkills
 import com.ryoserver.SkillSystems.SkillOpens.{SkillOpenCheck, SkillOpenData}
 import com.ryoserver.SkillSystems.SkillPoint.SkillPointData
-import org.bukkit.ChatColor
+import org.bukkit.ChatColor._
 import org.bukkit.entity.Player
 import org.bukkit.potion.PotionEffectType
 
@@ -16,28 +16,28 @@ trait SkillToggle {
   def effect(effectType:PotionEffectType,level:Int,sp:Int,skillName:String): Unit = {
     val openCheck = new SkillOpenCheck(ryoServerAssist)
     if (!openCheck.isOpened(skillName,p) && !openCheck.isTrueOpen(skillName,p)) {
-      p.sendMessage(ChatColor.RED + "このスキルは開放できません！")
+      p.sendMessage(RED + "このスキルは開放できません！")
       return
     } else if (openCheck.isTrueOpen(skillName,p) && !openCheck.isOpened(skillName,p)) {
       new SkillOpenData(ryoServerAssist).openSkill(p, skillName)
-      p.sendMessage(ChatColor.AQUA + "スキル:" + skillName + "を開放しました！")
+      p.sendMessage(AQUA + "スキル:" + skillName + "を開放しました！")
       new SelectSkillMenu(ryoServerAssist).openMenu(p)
       return
     }
     if (new SkillPointData(ryoServerAssist).getSkillPoint(p) < sp) {
       allEffectClear(p)
-      p.sendMessage(ChatColor.RED + "スキルポイントが足りないためスキルを起動できませんでした！")
+      p.sendMessage(RED + "スキルポイントが足りないためスキルを起動できませんでした！")
       return
     }
     val operation = new SkillOperation(p,skillName,ryoServerAssist)
     if (operation.isEnableSkill) {
       //スキルを無効化する
       operation.skillInvalidation()
-      p.sendMessage(ChatColor.AQUA + "スキルを無効化しました。")
+      p.sendMessage(AQUA + "スキルを無効化しました。")
     } else {
       //スキルの有効化
       operation.skillActivation(effectType,level,sp)
-      p.sendMessage(ChatColor.AQUA + "スキル:" + skillName + "を有効化しました。")
+      p.sendMessage(AQUA + "スキル:" + skillName + "を有効化しました。")
     }
   }
 
@@ -46,7 +46,7 @@ trait SkillToggle {
       enableSkills(p.getName).foreach(skillName => {
         new SkillOperation(p, skillName, ryoServerAssist).skillInvalidation()
       })
-      p.sendMessage(ChatColor.AQUA + "スキルをすべて無効化しました。")
+      p.sendMessage(AQUA + "スキルをすべて無効化しました。")
     }
   }
 
