ff32e0c21a42c44a1b9e9952679f7f5dcd7fb9a1
rito528 <rotoyutoriapp@gmail.com>
2021-09-27 20:12:44 +0900

    regionMenuの改善

--- src/main/scala/com/ryoserver/SimpleRegion/RegionMenu.scala
+++ src/main/scala/com/ryoserver/SimpleRegion/RegionMenu.scala
@@ -3,6 +3,7 @@ package com.ryoserver.SimpleRegion
 import com.ryoserver.Inventory.Item.getItem
 import org.bukkit.entity.Player
 import org.bukkit.{Bukkit, Material}
+import org.bukkit.ChatColor._
 
 import scala.jdk.CollectionConverters._
 
@@ -10,18 +11,18 @@ class RegionMenu {
 
   def menu(p:Player): Unit = {
     val inv = Bukkit.createInventory(null,9,"保護メニュー")
-    inv.setItem(2,getItem(Material.WOODEN_AXE,"木の斧を取得します。",List(
-      "保護方法",
-      "木の斧で始点を左クリックします。",
-      "終点を対角で右クリックします。",
-      "その後、ダイヤの斧をクリックします。").asJava))
-    inv.setItem(4,getItem(Material.DIAMOND_AXE,"保護をします。",List(
-      "クリックで保護を開始します。",
-      "結果がチャットに表示されます。"
+    inv.setItem(2,getItem(Material.WOODEN_AXE,s"${GREEN}木の斧を取得します。",List(
+      s"${GRAY}保護方法",
+      s"${GRAY}木の斧で始点を左クリックします。",
+      s"${GRAY}終点を対角で右クリックします。",
+      s"${GRAY}その後、ダイヤの斧をクリックします。").asJava))
+    inv.setItem(4,getItem(Material.DIAMOND_AXE,s"${GREEN}保護をします。",List(
+      s"${GRAY}クリックで保護を開始します。",
+      s"${GRAY}結果がチャットに表示されます。"
     ).asJava))
-    inv.setItem(6,getItem(Material.GOLDEN_AXE,"保護編集メニューを開きます。",List(
-      "クリックで保護編集メニューを開きます。",
-      "自分が管理者の保護範囲内にいる必要があります。"
+    inv.setItem(6,getItem(Material.GOLDEN_AXE,s"${GREEN}保護編集メニューを開きます。",List(
+      s"${GRAY}クリックで保護編集メニューを開きます。",
+      s"${GRAY}自分が管理者の保護範囲内にいる必要があります。"
     ).asJava))
     p.openInventory(inv)
   }
