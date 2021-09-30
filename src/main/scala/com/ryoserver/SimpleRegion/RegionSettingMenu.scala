ab167a6f87c4b54cc4b5029e7887155ae43c6772
rito528 <rotoyutoriapp@gmail.com>
2021-09-30 16:36:45 +0900

    フラグの設定が完成

--- src/main/scala/com/ryoserver/SimpleRegion/RegionSettingMenu.scala
+++ src/main/scala/com/ryoserver/SimpleRegion/RegionSettingMenu.scala
@@ -4,7 +4,8 @@ import com.ryoserver.Inventory.Item.getItem
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldedit.util.Location
 import com.sk89q.worldguard.WorldGuard
-import org.bukkit.{Bukkit, ChatColor, Material}
+import org.bukkit.{Bukkit, Material}
+import org.bukkit.ChatColor._
 import org.bukkit.entity.Player
 
 import scala.jdk.CollectionConverters._
@@ -17,7 +18,7 @@ class RegionSettingMenu {
     val set = regions.getApplicableRegions(new Location(BukkitAdapter.adapt(p.getWorld),p.getLocation().getX,p.getLocation().getY,
       p.getLocation().getZ).toVector.toBlockPoint)
     if (set.size() == 0) {
-      p.sendMessage(ChatColor.RED + "この場所は保護されていません！")
+      p.sendMessage(RED + "この場所は保護されていません！")
       return
     }
     var isOwner = false
@@ -25,11 +26,24 @@ class RegionSettingMenu {
       if (e.getOwners.contains(p.getUniqueId)) isOwner = true
     })
     if (!isOwner) {
-      p.sendMessage(ChatColor.RED + "あなたの保護範囲ではありません！")
+      p.sendMessage(RED + "あなたの保護範囲ではありません！")
       return
     }
     val inv = Bukkit.createInventory(null,54,"保護設定メニュー")
-    inv.setItem(1,getItem(Material.TNT,"保護を削除します。",List("取扱注意！","保護範囲を削除します。").asJava))
+    inv.setItem(1,getItem(Material.TNT,s"${RED}${BOLD}保護を削除します。",
+      List(s"${RED}${BOLD}取扱注意！",s"${RED}${BOLD}保護範囲を削除します。").asJava))
+    inv.setItem(3,getItem(Material.OAK_DOOR,s"${GREEN}フラグ:useを切り替えます。",
+      List(s"${GRAY}ドアやボタンの使用を許可します。").asJava))
+    inv.setItem(5,getItem(Material.OAK_BUTTON,s"${GREEN}フラグ:interactを切り替えます。",
+      List(s"${GRAY}スイッチの使用を許可します。").asJava))
+    inv.setItem(7,getItem(Material.CHEST,s"${GREEN}フラグ:chest-accessを切り替えます。",
+      List(s"${GRAY}チェストの使用を許可します。").asJava))
+    inv.setItem(19,getItem(Material.WHITE_BED,s"${GREEN}フラグ:sleepを許可します。",
+      List(s"${GRAY}ベットで眠る許可をします。").asJava))
+    inv.setItem(21,getItem(Material.MINECART,s"${GREEN}フラグ:vehicle-placeを許可します。",
+      List(s"${GRAY}トロッコ、ボードの設置を許可します。").asJava))
+    inv.setItem(23,getItem(Material.OAK_BOAT,s"${GREEN}フラグ:vehicle-destroyを許可します。",
+      List(s"${GRAY}トロッコ、ボードの破壊を許可します。").asJava))
     p.openInventory(inv)
   }
 
--- src/main/scala/com/ryoserver/SimpleRegion/RegionSettingMenuEvent.scala
+++ src/main/scala/com/ryoserver/SimpleRegion/RegionSettingMenuEvent.scala
@@ -3,10 +3,13 @@ package com.ryoserver.SimpleRegion
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldedit.util.Location
 import com.sk89q.worldguard.WorldGuard
-import org.bukkit.ChatColor
+import com.sk89q.worldguard.protection.ApplicableRegionSet
+import com.sk89q.worldguard.protection.flags.{Flags, StateFlag}
+import com.sk89q.worldguard.protection.regions.ProtectedRegion
+import org.bukkit.ChatColor._
 import org.bukkit.entity.Player
-import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.{EventHandler, Listener}
 
 class RegionSettingMenuEvent extends Listener {
 
@@ -15,19 +18,37 @@ class RegionSettingMenuEvent extends Listener {
     if (e.getView.getTitle != "保護設定メニュー") return
     e.setCancelled(true)
     val p = e.getWhoClicked.asInstanceOf[Player]
+    val container = WorldGuard.getInstance().getPlatform.getRegionContainer
+    val regions = container.get(BukkitAdapter.adapt(p.getWorld))
+    val set = regions.getApplicableRegions(new Location(BukkitAdapter.adapt(p.getWorld),p.getLocation().getX,p.getLocation().getY,
+      p.getLocation().getZ).toVector.toBlockPoint)
     e.getSlot match {
       case 1 =>
-        val container = WorldGuard.getInstance().getPlatform.getRegionContainer
-        val regions = container.get(BukkitAdapter.adapt(p.getWorld))
-        val set = regions.getApplicableRegions(new Location(BukkitAdapter.adapt(p.getWorld),p.getLocation().getX,p.getLocation().getY,
-          p.getLocation().getZ).toVector.toBlockPoint)
         set.getRegions.forEach(region => {
           regions.removeRegion(region.getId)
-          p.sendMessage(ChatColor.AQUA + "保護:" + region.getId + "を削除しました。")
+          p.sendMessage(AQUA + "保護:" + region.getId + "を削除しました。")
         })
+      case 3 => toggleFlag(set,Flags.USE,p)
+      case 5 => toggleFlag(set,Flags.INTERACT,p)
+      case 7 => toggleFlag(set,Flags.CHEST_ACCESS,p)
+      case 19 => toggleFlag(set,Flags.SLEEP,p)
+      case 21 => toggleFlag(set,Flags.PLACE_VEHICLE,p)
+      case 23 => toggleFlag(set,Flags.DESTROY_VEHICLE,p)
       case _ =>
 
     }
+  }
+
+  def toggleFlag(set:ApplicableRegionSet, flag:StateFlag, p:Player): Unit = {
+    set.getRegions.forEach(region => {
+      if (region.getFlags.getOrDefault(flag,"DENY").toString == "DENY") {
+        region.setFlag(flag, StateFlag.State.ALLOW)
+        p.sendMessage(s"${AQUA}保護:${region.getId}のフラグ${flag.getName}を有効化しました。")
+      } else {
+        region.setFlag(flag, StateFlag.State.DENY)
+        p.sendMessage(s"${AQUA}保護:${region.getId}のフラグ${flag.getName}を無効化しました。")
+      }
+    })
   }
 
 }
