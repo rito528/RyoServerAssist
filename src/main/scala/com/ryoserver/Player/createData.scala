34c93ddd657ac9f05b0255106ad886e4e7dcd4a4
rito528 <rotoyutoriapp@gmail.com>
2021-10-01 09:26:54 +0900

    初参加時にアイテムが配布されるように

--- src/main/scala/com/ryoserver/Player/createData.scala
+++ src/main/scala/com/ryoserver/Player/createData.scala
@@ -3,8 +3,9 @@ package com.ryoserver.Player
 import com.ryoserver.RyoServerAssist
 import com.ryoserver.SkillSystems.SkillPoint.SkillPointCal
 import com.ryoserver.util.SQL
-import org.bukkit.{Bukkit, ChatColor, Sound}
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
+import org.bukkit.{Bukkit, ChatColor, Sound}
 
 class createData(ryoServerAssist: RyoServerAssist) {
 
@@ -24,6 +25,18 @@ class createData(ryoServerAssist: RyoServerAssist) {
       sql.executeSQL(s"INSERT INTO Players (UUID,lastLogin,loginDays,consecutiveLoginDays," +
         s"lastDistributionReceived,EXP,Level,questClearTimes,gachaTickets,SkillPoint,SkillOpenPoint) " +
         s"VALUES ('${p.getUniqueId}',NOW(),1,1,(SELECT MAX(id) FROM Distribution),0,0,0,0,${new SkillPointCal().getMaxSkillPoint(0)},0);")
+      val inv = p.getInventory
+      val items = sql.executeQuery("SELECT ItemStack FROM firstJoinItems;")
+      var counter = 0
+      if (items.next()) {
+        val invData = items.getString("ItemStack").split(";")
+        val config = new YamlConfiguration
+        invData.foreach(material => {
+          config.loadFromString(material)
+          inv.setItem(counter,config.getItemStack("i",null))
+          counter += 1
+        })
+      }
       Bukkit.broadcastMessage(ChatColor.AQUA + p.getName + "さんが初参加しました！")
       Bukkit.getOnlinePlayers.forEach(p => p.playSound(p.getLocation(),Sound.BLOCK_NOTE_BLOCK_BELL,1,1))
     }
