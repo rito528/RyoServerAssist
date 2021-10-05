b7a0a1e62ce5b6c41940b177fb01daff6fdeeb47
rito528 <rotoyutoriapp@gmail.com>
2021-10-05 20:16:32 +0900

    テーブルの構成を変更

--- src/main/scala/com/ryoserver/Player/createData.scala
+++ src/main/scala/com/ryoserver/Player/createData.scala
@@ -19,7 +19,7 @@ class createData(ryoServerAssist: RyoServerAssist) {
     val table_rs = sql.executeQuery("SHOW TABLES LIKE 'Players';")
     //UUID=UUID,lastLogin=最終ログイン,loginDays=ログイン日数,consecutiveLoginDays=連続ログイン日数,lastDistributionReceived=最後に受け取った配布番号)
     if (!table_rs.next()) sql.executeSQL("CREATE TABLE Players(UUID Text,lastLogin DATETIME,loginDays INT,consecutiveLoginDays INT," +
-      "lastDistributionReceived INT,EXP DOUBLE,Level INT,questClearTimes INT,gachaTickets INT,SkillPoint INT,SkillOpenPoint INT,OpenedSkills TEXT);")
+      "lastDistributionReceived INT,EXP DOUBLE,Level INT,questClearTimes INT,gachaTickets INT,SkillPoint INT,SkillOpenPoint INT,OpenedSkills TEXT,OpenedTitles TEXT);")
     val user_rs = sql.executeQuery(s"SELECT UUID FROM Players WHERE UUID='${p.getUniqueId.toString}';")
     if (!user_rs.next()) {
       sql.executeSQL(s"INSERT INTO Players (UUID,lastLogin,loginDays,consecutiveLoginDays," +
