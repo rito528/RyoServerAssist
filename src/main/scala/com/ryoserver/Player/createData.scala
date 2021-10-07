264b15926a702a11008b94a215254878e5bce822
rito528 <rotoyutoriapp@gmail.com>
2021-10-07 20:15:09 +0900

    Playersテーブルの構成を変更

--- src/main/scala/com/ryoserver/Player/createData.scala
+++ src/main/scala/com/ryoserver/Player/createData.scala
@@ -19,13 +19,15 @@ class createData(ryoServerAssist: RyoServerAssist) {
     val table_rs = sql.executeQuery("SHOW TABLES LIKE 'Players';")
     //UUID=UUID,lastLogin=最終ログイン,loginDays=ログイン日数,consecutiveLoginDays=連続ログイン日数,lastDistributionReceived=最後に受け取った配布番号)
     if (!table_rs.next()) sql.executeSQL("CREATE TABLE Players(UUID Text,lastLogin DATETIME,loginDays INT,consecutiveLoginDays INT," +
-      "lastDistributionReceived INT,EXP DOUBLE,Level INT,questClearTimes INT,gachaTickets INT,SkillPoint INT,SkillOpenPoint INT,OpenedSkills TEXT,OpenedTitles TEXT);")
+      "lastDistributionReceived INT,EXP DOUBLE,Level INT,questClearTimes INT,gachaTickets INT,gachaPullNumber INT,SkillPoint INT,SkillOpenPoint INT,OpenedSkills TEXT,OpenedTitles TEXT);")
     val user_rs = sql.executeQuery(s"SELECT UUID FROM Players WHERE UUID='${p.getUniqueId.toString}';")
     if (!user_rs.next()) {
       sql.executeSQL(s"INSERT INTO Players (UUID,lastLogin,loginDays,consecutiveLoginDays," +
-        s"lastDistributionReceived,EXP,Level,questClearTimes,gachaTickets,SkillPoint,SkillOpenPoint) " +
-        s"VALUES ('${p.getUniqueId}',NOW(),1,1,(SELECT MAX(id) FROM Distribution),0,0,0,0,${new SkillPointCal().getMaxSkillPoint(0)},0);")
+        s"lastDistributionReceived,EXP,Level,questClearTimes,gachaTickets,gachaPullNumber,SkillPoint,SkillOpenPoint) " +
+        s"VALUES ('${p.getUniqueId}',NOW(),1,1,(SELECT MAX(id) FROM Distribution),0,0,0,0,0,${new SkillPointCal().getMaxSkillPoint(0)},0);")
       val inv = p.getInventory
+      val rs = sql.executeQuery("SHOW TABLES LIKE 'firstJoinItems';")
+      if (!rs.next()) sql.executeSQL("CREATE TABLE firstJoinItems(id INT AUTO_INCREMENT,ItemStack TEXT,PRIMARY KEY(`id`));")
       val items = sql.executeQuery("SELECT ItemStack FROM firstJoinItems;")
       var counter = 0
       if (items.next()) {
