79c3af5d66718839a7fd863327db3e12ce5c6677
rito528 <rotoyutoriapp@gmail.com>
2021-10-10 00:29:35 +0900

    ワールド生成時の岩盤生成範囲の変更

--- src/main/scala/com/ryoserver/World/Regeneration/Regeneration.scala
+++ src/main/scala/com/ryoserver/World/Regeneration/Regeneration.scala
@@ -3,6 +3,9 @@ package com.ryoserver.World.Regeneration
 import com.onarandombox.MultiverseCore.MultiverseCore
 import com.onarandombox.MultiversePortals.MultiversePortals
 import com.ryoserver.RyoServerAssist
+import com.sk89q.worldedit.WorldEdit
+import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldedit.math.BlockVector3
 import org.bukkit.Bukkit.getConsoleSender
 import org.bukkit.World.Environment
 import org.bukkit._
@@ -15,7 +18,7 @@ class Regeneration(ryoServerAssist: RyoServerAssist) {
   private def isFriday: Boolean = {
     val calendar = Calendar.getInstance()
     calendar.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"))
-    if (calendar.get(Calendar.DAY_OF_WEEK) == 7) return true
+    if (calendar.get(Calendar.DAY_OF_WEEK) == 1) return true
     false
   }
 
@@ -36,11 +39,27 @@ class Regeneration(ryoServerAssist: RyoServerAssist) {
       worldManager.addWorld(world,worldType,null,WorldType.NORMAL,true,null)
       worldManager.getMVWorld(world).getCBWorld.setGameRule(GameRule.KEEP_INVENTORY.asInstanceOf[GameRule[Any]],true)
       worldManager.getMVWorld(world).setDifficulty(Difficulty.HARD)
-      val spawnLocation = worldManager.getMVWorld(world).getCBWorld.getSpawnLocation
       val random = SecureRandom.getInstance("SHA1PRNG")
-      worldManager.getMVWorld(world).getCBWorld.setSpawnLocation(worldManager.getMVWorld(world).getCBWorld.getHighestBlockAt(random.nextInt(1000),random.nextInt(1000)).getLocation)
-      spawnLocation.add(0,-1,0).getBlock.setType(Material.BEDROCK)
-      worldManager.getMVWorld(world).getCBWorld.setSpawnLocation(spawnLocation)
+      val x = random.nextInt(500)
+      val z = random.nextInt(500)
+//      worldManager.getMVWorld(world).getCBWorld.setSpawnLocation(new Location(worldManager.getMVWorld(world).getCBWorld,x,64,z))
+      val landLocation = new Location(worldManager.getMVWorld(world).getCBWorld,x,64,z)
+      //val spawnLocation = worldManager.getMVWorld(world).getCBWorld.getSpawnLocation
+      for (y <- 0 until 60) {
+        for (x <- 0 until 40) {
+          for (z <- 0 until 40) {
+            val minusLoc = landLocation.clone().add(-20, 0, -20)
+            minusLoc.add(x, y, z).getBlock.setType(Material.AIR)
+          }
+        }
+      }
+      for (x <- 0 until 40) {
+        for (z <- 0 until 40){
+          val minusLoc = landLocation.clone().add(-20, 0 ,-20)
+          minusLoc.add(x, 0, z).getBlock.setType(Material.BEDROCK)
+        }
+      }
+      worldManager.getMVWorld(world).setSpawnLocation(new Location(worldManager.getMVWorld(world).getCBWorld,x,65,z))
       Bukkit.dispatchCommand(getConsoleSender,s"wb $world set 5000 5000 spawn")
       Bukkit.dispatchCommand(getConsoleSender,"wb shape square")
       portals.getPortalManager.getPortal(s"worldTo$world").setExactDestination(Bukkit.getWorld(world).getSpawnLocation)
