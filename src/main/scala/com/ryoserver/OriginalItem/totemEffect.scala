c8f89444c5b4a9faeaf3e523612ea81cc46e74be
rito528 <rotoyutoriapp@gmail.com>
2021-10-11 16:15:25 +0900

    折れた英雄の剣の機能追加

---
+++ src/main/scala/com/ryoserver/OriginalItem/totemEffect.scala
@@ -0,0 +1,35 @@
+package com.ryoserver.OriginalItem
+
+import org.bukkit.{Material, Sound}
+import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.{EventHandler, Listener}
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.Damageable
+import org.bukkit.potion.{PotionEffect, PotionEffectType}
+
+class totemEffect extends Listener {
+
+  @EventHandler
+  def onDamage(e: EntityDamageEvent): Unit = {
+    if (!e.getEntity.isInstanceOf[Player]) return
+    val p = e.getEntity.asInstanceOf[Player]
+    val item = p.getInventory.getItemInOffHand
+    if (p.getHealth - e.getDamage < 1 && item.getType == OriginalItems.oretaEiyuNoKen.getType && item.getItemMeta.getItemFlags == OriginalItems.oretaEiyuNoKen.getItemMeta.getItemFlags ) {
+      e.setCancelled(true)
+      p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,20 * 40, 2))
+      p.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,20 * 40, 2))
+      p.setHealth(5.0)
+      val meta = item.getItemMeta
+      if (meta.asInstanceOf[Damageable].getDamage >= 250) {
+        p.getInventory.setItemInOffHand(new ItemStack(Material.AIR,0))
+        p.playSound(p.getLocation,Sound.ENTITY_ITEM_BREAK,1,1)
+      } else {
+        meta.asInstanceOf[Damageable].setDamage(meta.asInstanceOf[Damageable].getDamage + 1)
+      }
+      item.setItemMeta(meta)
+      p.playSound(p.getLocation,Sound.ITEM_TOTEM_USE,1,1)
+    }
+  }
+
+}
--- src/main/scala/com/ryoserver/RyoServerAssist.scala
+++ src/main/scala/com/ryoserver/RyoServerAssist.scala
@@ -10,7 +10,7 @@ import com.ryoserver.Home.Home
 import com.ryoserver.Level.LevelCommand
 import com.ryoserver.Menu.{MenuCommand, MenuEvent}
 import com.ryoserver.Notification.Notification
-import com.ryoserver.OriginalItem.OriginalItemCommands
+import com.ryoserver.OriginalItem.{OriginalItemCommands, totemEffect}
 import com.ryoserver.Player.{FirstJoinSettingCommand, FirstJoinSettingEvent, PlayerEvents, playerDataLoader}
 import com.ryoserver.Quest.{QuestSelectInventoryEvent, loadQuests, suppressionEvent}
 import com.ryoserver.World.SimpleRegion.{RegionCommand, RegionMenuEvent, RegionSettingMenuEvent}
@@ -86,7 +86,8 @@ class RyoServerAssist extends JavaPlugin {
       new GachaAddItemInventoryEvent(this),
       new DustBoxInventoryEvent,
       new FirstJoinSettingEvent(this),
-      new ElevatorEvent
+      new ElevatorEvent,
+      new totemEffect
     ).foreach(listener => this.getServer.getPluginManager.registerEvents(listener,this))
 
     /*
