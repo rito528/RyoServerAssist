2a6089147a723ae3e1d38d3df43699b706e4a0e8
rito528 <rotoyutoriapp@gmail.com>
2021-11-12 23:18:40 +0900

    次のガチャ券が貰えるまでの経験値量を小数点第１位で切り捨てるように仕様変更

--- src/main/scala/com/ryoserver/Menu/CreateMenu.scala
+++ src/main/scala/com/ryoserver/Menu/CreateMenu.scala
@@ -46,7 +46,7 @@ class CreateMenu(ryoServerAssist: RyoServerAssist) extends Menu {
       s"${GRAY}ガチャ券はEXPが100毎に1枚、または",
       s"${GRAY}レベルが10上がる毎に32枚手に入ります。",
       s"${GRAY}受け取れるガチャ券の枚数:" + new GetData(ryoServerAssist).getTickets(p) + "枚",
-      s"${GRAY}次のガチャ券まであと" + (100 - new GetPlayerData(ryoServerAssist).getPlayerExp(p) % 100)))
+      s"${GRAY}次のガチャ券まであと" + String.format("%.1f",(100 - new GetPlayerData(ryoServerAssist).getPlayerExp(p) % 100))))
     setItem(5, 5, Material.HONEY_BOTTLE, effect = true, s"${GREEN}ガチャ特等アイテム交換画面を開きます。", List(s"${GRAY}クリックで開きます。"))
     setItem(1, 6, Material.ENDER_PEARL, effect = false, s"${GREEN}現在いるワールドのスポーン地点に移動します。", List(s"${GRAY}クリックで移動します。"))
     setItem(2, 6, Material.COMPASS, effect = false, s"${GREEN}スポーン地点に移動します。", List(s"${GRAY}クリックで移動します。"))
