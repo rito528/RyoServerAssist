c5f11ec03e5ff0a19ae05a3321bb618f18cd4e99
rito528 <rotoyutoriapp@gmail.com>
2021-12-04 19:40:55 +0900

    オリジナルアイテムに対して砥石が使えないように仕様変更

--- src/main/scala/com/ryoserver/OriginalItem/AnvilRepairEvent.scala
+++ src/main/scala/com/ryoserver/OriginalItem/AnvilRepairEvent.scala
@@ -1,8 +1,8 @@
 package com.ryoserver.OriginalItem
 
 import com.ryoserver.OriginalItem.OriginalItems.{itemList, metaList}
-import org.bukkit.event.inventory.PrepareAnvilEvent
-import org.bukkit.event.{EventHandler, Listener}
+import org.bukkit.event.inventory.{InventoryClickEvent, InventoryType, PrepareAnvilEvent}
+import org.bukkit.event.{Event, EventHandler, Listener}
 
 class AnvilRepairEvent extends Listener {
 
@@ -11,6 +11,18 @@ class AnvilRepairEvent extends Listener {
     val item = e.getInventory.getItem(0)
     if (item == null) return
     if (itemList.contains(item) || metaList.contains(item.getItemMeta.getItemFlags)) e.getInventory.setRepairCost(100)
+  }
+
+  @EventHandler
+  def grindStone(e: InventoryClickEvent): Unit = {
+    if (e.getClickedInventory.getType != InventoryType.GRINDSTONE) return
+    val item1 = e.getInventory.getItem(0)
+    val item2 = e.getInventory.getItem(1)
+    if (item1 == null && item2 == null) return
+    if ((itemList.contains(item1) || itemList.contains(item2) ||
+      metaList.contains(item1.getItemMeta.getItemFlags) || metaList.contains(item2.getItemMeta.getItemFlags)) && e.getSlot == 2) {
+      e.setResult(Event.Result.DENY)
+    }
   }
 
 }
