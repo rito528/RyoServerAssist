7bf4fa49e560ab4c2805c2d3d057062fafe8e75a
rito528 <rotoyutoriapp@gmail.com>
2021-12-07 06:51:36 +0900

    破壊スキルを利用した際に耐久値が考慮されるように

--- src/main/scala/com/ryoserver/SkillSystems/Skill/DestructionSkill.scala
+++ src/main/scala/com/ryoserver/SkillSystems/Skill/DestructionSkill.scala
@@ -1,9 +1,14 @@
 package com.ryoserver.SkillSystems.Skill
 
-import org.bukkit.Location
+import org.bukkit.{Location, Sound}
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.{EventHandler, Listener}
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.Damageable
+
+import java.security.SecureRandom
 
 class DestructionSkill extends Listener {
 
@@ -36,20 +41,19 @@ class DestructionSkill extends Listener {
    */
   def nsBreak(p:Player,location: Location,x:Int,y:Int,z:Int,lxm:Int,lym:Int): Unit = {
     location.add(x,y,z)
-    println("ns")
     for (lx <- 0 to lxm) {
       for (ly <- 0 to lym) {
-        println(lx + ":" + ly)
         val cloneLocation = location.clone()
         cloneLocation.add(lx, ly, 0)
         cloneLocation.getBlock.breakNaturally(p.getInventory.getItemInMainHand)
+        itemAddDamage(p)
       }
     }
   }
 
   /*
-    東西で破壊する場合のメソッド
-   */
+   東西で破壊する場合のメソッド
+  */
   def ewBreak(p:Player,location: Location,x:Int,y:Int,z:Int,lym:Int,lzm:Int): Unit = {
     location.add(x,y,z)
     for (lz <- 0 to lzm) {
@@ -57,7 +61,30 @@ class DestructionSkill extends Listener {
         val cloneLocation = location.clone()
         cloneLocation.add(0, ly, lz)
         cloneLocation.getBlock.breakNaturally(p.getInventory.getItemInMainHand)
+        itemAddDamage(p)
       }
+    }
+  }
+
+  /*
+    アイテム耐久値の計算
+   */
+  private def itemAddDamage(p: Player): Unit = {
+    if (p.getInventory.getItemInMainHand != null && p.getInventory.getItemInMainHand.getType.getMaxDurability != 0) {
+      val meta = p.getInventory.getItemInMainHand.getItemMeta
+      val random = SecureRandom.getInstance("SHA1PRNG")
+      val durabilityLevel = meta.getEnchantLevel(Enchantment.DURABILITY)
+      val probability = 100 / (durabilityLevel + 1)
+      if (random.nextInt(100) <= probability) {
+        val itemDamage = meta.asInstanceOf[Damageable].getDamage
+        if (itemDamage + 1 > p.getInventory.getItemInMainHand.getType.getMaxDurability) {
+          p.getInventory.setItemInMainHand(null)
+          p.playSound(p.getLocation, Sound.ENTITY_ITEM_BREAK, 1, 1)
+        } else {
+          meta.asInstanceOf[Damageable].setDamage(itemDamage + 1)
+        }
+      }
+      p.getInventory.getItemInMainHand.setItemMeta(meta)
     }
   }
 
