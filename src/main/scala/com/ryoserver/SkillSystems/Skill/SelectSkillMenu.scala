f6589e5311582d740ceaca4f0c23a7342a6b4ff8
rito528 <rotoyutoriapp@gmail.com>
2021-12-08 19:37:44 +0900

    スキルメニューを詰めて表示するように変更

--- src/main/scala/com/ryoserver/SkillSystems/Skill/SelectSkillMenu.scala
+++ src/main/scala/com/ryoserver/SkillSystems/Skill/SelectSkillMenu.scala
@@ -19,58 +19,58 @@ class SelectSkillMenu(ryoServerAssist: RyoServerAssist) extends Menu {
     p = player
     val check = new SkillOpenCheck(ryoServerAssist)
     val skillData = new SkillOpenData(ryoServerAssist)
-    setItem(2, 2, if (check.isOpened(SkillNames.head, p)) Material.SHIELD else Material.BEDROCK, effect = false,
+    setItem(1, 1, if (check.isOpened(SkillNames.head, p)) Material.SHIELD else Material.BEDROCK, effect = false,
       s"${GREEN}[基本スキル]${SkillNames.head}", List(s"${GRAY}耐性１の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames.head, p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         s"${GRAY}スキルポイントコスト:300"))
-    setItem(4, 2, if (check.isOpened(SkillNames(1), p)) Material.IRON_BOOTS else Material.BEDROCK, effect = false,
+    setItem(2, 1, if (check.isOpened(SkillNames(1), p)) Material.IRON_BOOTS else Material.BEDROCK, effect = false,
       s"${GREEN}[基本スキル]${SkillNames(1)}", List(s"${GRAY}移動速度上昇1の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(1), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         s"${GRAY}スキルポイントコスト:300"))
-    setItem(6, 2, if (check.isOpened(SkillNames(2), p)) Material.RABBIT_FOOT else Material.BEDROCK, effect = false,
+    setItem(3, 1, if (check.isOpened(SkillNames(2), p)) Material.RABBIT_FOOT else Material.BEDROCK, effect = false,
       s"${GREEN}[基本スキル]${SkillNames(2)}", List(s"${GRAY}跳躍力上昇1の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(2), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         s"${GRAY}スキルポイントコスト:300"))
-    setItem(8, 2, if (check.isOpened(SkillNames(3), p)) Material.IRON_SWORD else Material.BEDROCK, effect = false,
+    setItem(4, 1, if (check.isOpened(SkillNames(3), p)) Material.IRON_SWORD else Material.BEDROCK, effect = false,
       s"${GREEN}[基本スキル]${SkillNames(3)}", List(s"${GRAY}攻撃力上昇1の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(3), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         s"${GRAY}スキルポイントコスト:300"))
-    setItem(2, 4, if (check.isOpened(SkillNames(4), p)) Material.IRON_PICKAXE else Material.BEDROCK, effect = false,
+    setItem(5, 1, if (check.isOpened(SkillNames(4), p)) Material.IRON_PICKAXE else Material.BEDROCK, effect = false,
       s"${GREEN}[基本スキル]${SkillNames(4)}", List(s"${GRAY}採掘速度上昇1の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(4), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         s"${GRAY}スキルポイントコスト:300"))
-    setItem(4, 4, if (check.isOpened(SkillNames(5), p)) Material.ENCHANTED_GOLDEN_APPLE else Material.BEDROCK, effect = false,
+    setItem(6, 1, if (check.isOpened(SkillNames(5), p)) Material.ENCHANTED_GOLDEN_APPLE else Material.BEDROCK, effect = false,
       s"${GREEN}[基本スキル]${SkillNames(5)}", List(s"${GRAY}再生能力1の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(5), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         s"${GRAY}スキルポイントコスト:300"))
-    setItem(6, 4, if (check.isOpened(SkillNames(6), p)) Material.ELYTRA else Material.BEDROCK, effect = true,
+    setItem(1, 2, if (check.isOpened(SkillNames(6), p)) Material.ELYTRA else Material.BEDROCK, effect = true,
       s"${GREEN}${SkillNames(6)}",
       List(s"${GRAY}低速落下の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(6), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         if (!check.isOpened(SkillNames(6), p)) s"${GRAY}基本スキルをすべて開放" else "",
         s"${GRAY}スキルポイントコスト:600"))
-    setItem(8, 4, if (check.isOpened(SkillNames(7), p)) Material.ENDER_EYE else Material.BEDROCK, effect = true,
+    setItem(2, 2, if (check.isOpened(SkillNames(7), p)) Material.ENDER_EYE else Material.BEDROCK, effect = true,
       s"${GREEN}${SkillNames(7)}",
       List(s"${GRAY}暗視の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(7), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         if (!check.isOpened(SkillNames(7), p)) s"${GRAY}基本スキルをすべて開放" else "",
         s"${GRAY}スキルポイントコスト:600"))
-    setItem(2, 6, if (check.isOpened(SkillNames(8), p)) Material.LAVA_BUCKET else Material.BEDROCK, effect = true,
+    setItem(3, 2, if (check.isOpened(SkillNames(8), p)) Material.LAVA_BUCKET else Material.BEDROCK, effect = true,
       s"${GREEN}${SkillNames(8)}",
       List(s"${GRAY}耐火の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(8), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         if (!check.isOpened(SkillNames(8), p)) s"${GRAY}基本スキルをすべて開放" else "",
         s"${GRAY}スキルポイントコスト:600"))
-    setItem(4, 6, if (check.isOpened(SkillNames(9), p)) Material.WATER_BUCKET else Material.BEDROCK, effect = true,
+    setItem(4, 2, if (check.isOpened(SkillNames(9), p)) Material.WATER_BUCKET else Material.BEDROCK, effect = true,
       s"${GREEN}${SkillNames(9)}",
       List(s"${GRAY}水中呼吸の効果が付与されます。",
         GRAY + (if (check.isOpened(SkillNames(9), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
         if (!check.isOpened(SkillNames(9), p)) s"${GRAY}基本スキルをすべて開放" else "",
         s"${GRAY}スキルポイントコスト:600"))
-    setSkullItem(6, 6, p, s"${GREEN}クリックですべてのスキル選択を解除できます。", List(
+    setSkullItem(8, 6, p, s"${GREEN}クリックですべてのスキル選択を解除できます。", List(
       s"${GRAY}現在保有中のスキル開放ポイント:" + skillData.getSkillOpenPoint(p)
     ))
-    setItem(8, 6, Material.MAGENTA_GLAZED_TERRACOTTA, effect = false, s"${GREEN}メニューに戻る", List(s"${GRAY}メニューに戻ります。"))
+    setItem(1, 6, Material.MAGENTA_GLAZED_TERRACOTTA, effect = false, s"${GREEN}メニューに戻る", List(s"${GRAY}メニューに戻ります。"))
     registerMotion(motion)
     open()
   }
@@ -78,20 +78,20 @@ class SelectSkillMenu(ryoServerAssist: RyoServerAssist) extends Menu {
   def motion(player: Player, index: Int): Unit = {
     val toggle = new skillToggleClass(player, ryoServerAssist)
     index match {
-      case 10 => toggle.effect(PotionEffectType.DAMAGE_RESISTANCE, 0, 300, SkillNames.head)
-      case 12 => toggle.effect(PotionEffectType.SPEED, 0, 300, SkillNames(1))
-      case 14 => toggle.effect(PotionEffectType.JUMP, 0, 300, SkillNames(2))
-      case 16 => toggle.effect(PotionEffectType.INCREASE_DAMAGE, 0, 300, SkillNames(3))
-      case 28 => toggle.effect(PotionEffectType.FAST_DIGGING, 0, 300, SkillNames(4))
-      case 30 => toggle.effect(PotionEffectType.REGENERATION, 0, 300, SkillNames(5))
-      case 32 => toggle.effect(PotionEffectType.SLOW_FALLING, 0, 600, SkillNames(6))
-      case 34 => toggle.effect(PotionEffectType.NIGHT_VISION, 0, 600, SkillNames(7))
-      case 46 => toggle.effect(PotionEffectType.FIRE_RESISTANCE, 0, 600, SkillNames(8))
-      case 48 => toggle.effect(PotionEffectType.WATER_BREATHING, 0, 600, SkillNames(9))
-      case 50 =>
+      case 0 => toggle.effect(PotionEffectType.DAMAGE_RESISTANCE, 0, 300, SkillNames.head)
+      case 1 => toggle.effect(PotionEffectType.SPEED, 0, 300, SkillNames(1))
+      case 2 => toggle.effect(PotionEffectType.JUMP, 0, 300, SkillNames(2))
+      case 3 => toggle.effect(PotionEffectType.INCREASE_DAMAGE, 0, 300, SkillNames(3))
+      case 4 => toggle.effect(PotionEffectType.FAST_DIGGING, 0, 300, SkillNames(4))
+      case 5 => toggle.effect(PotionEffectType.REGENERATION, 0, 300, SkillNames(5))
+      case 9 => toggle.effect(PotionEffectType.SLOW_FALLING, 0, 600, SkillNames(6))
+      case 10 => toggle.effect(PotionEffectType.NIGHT_VISION, 0, 600, SkillNames(7))
+      case 11 => toggle.effect(PotionEffectType.FIRE_RESISTANCE, 0, 600, SkillNames(8))
+      case 12 => toggle.effect(PotionEffectType.WATER_BREATHING, 0, 600, SkillNames(9))
+      case 45 => new RyoServerMenu1(ryoServerAssist).menu(p)
+      case 52 =>
         toggle.allEffectClear(p)
         p.sendMessage(AQUA + "スキルをすべて無効化しました。")
-      case 52 => new RyoServerMenu1(ryoServerAssist).menu(p)
       case _ =>
     }
   }
