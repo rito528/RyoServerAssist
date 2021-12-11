e2962f21710afe0b1ebc2704f1ef2ada922a0838
rito528 <rotoyutoriapp@gmail.com>
2021-12-11 13:48:13 +0900

    各種レベル2のスキルを追加

--- src/main/scala/com/ryoserver/SkillSystems/Skill/SelectSkillMenu.scala
+++ src/main/scala/com/ryoserver/SkillSystems/Skill/SelectSkillMenu.scala
@@ -70,6 +70,31 @@ class SelectSkillMenu(ryoServerAssist: RyoServerAssist) extends Menu {
     setSkullItem(8, 6, p, s"${GREEN}クリックですべてのスキル選択を解除できます。", List(
       s"${GRAY}現在保有中のスキル開放ポイント:" + skillData.getSkillOpenPoint(p)
     ))
+
+    setItem(1, 3, if (check.isOpened(SkillNames(10), p)) Material.SHIELD else Material.BEDROCK, effect = false,
+      s"${GREEN}${SkillNames(10)}", List(s"${GRAY}耐性2の効果が付与されます。",
+        GRAY + (if (check.isOpened(SkillNames.head, p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
+        s"${GRAY}スキルポイントコスト:600"))
+    setItem(2, 3, if (check.isOpened(SkillNames(11), p)) Material.IRON_BOOTS else Material.BEDROCK, effect = false,
+      s"${GREEN}${SkillNames(11)}", List(s"${GRAY}移動速度上昇2の効果が付与されます。",
+        GRAY + (if (check.isOpened(SkillNames(11), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
+        s"${GRAY}スキルポイントコスト:600"))
+    setItem(3, 3, if (check.isOpened(SkillNames(12), p)) Material.RABBIT_FOOT else Material.BEDROCK, effect = false,
+      s"${GREEN}${SkillNames(12)}", List(s"${GRAY}跳躍力上昇2の効果が付与されます。",
+        GRAY + (if (check.isOpened(SkillNames(12), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
+        s"${GRAY}スキルポイントコスト:600"))
+    setItem(4, 3, if (check.isOpened(SkillNames(13), p)) Material.IRON_SWORD else Material.BEDROCK, effect = false,
+      s"${GREEN}${SkillNames(13)}", List(s"${GRAY}攻撃力上昇2の効果が付与されます。",
+        GRAY + (if (check.isOpened(SkillNames(13), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
+        s"${GRAY}スキルポイントコスト:600"))
+    setItem(5, 3, if (check.isOpened(SkillNames(14), p)) Material.IRON_PICKAXE else Material.BEDROCK, effect = false,
+      s"${GREEN}${SkillNames(14)}", List(s"${GRAY}採掘速度上昇2の効果が付与されます。",
+        GRAY + (if (check.isOpened(SkillNames(14), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
+        s"${GRAY}スキルポイントコスト:600"))
+    setItem(6, 3, if (check.isOpened(SkillNames(15), p)) Material.ENCHANTED_GOLDEN_APPLE else Material.BEDROCK, effect = false,
+      s"${GREEN}${SkillNames(15)}", List(s"${GRAY}再生能力2の効果が付与されます。",
+        GRAY + (if (check.isOpened(SkillNames(15), p)) "開放済みです。" else "解放条件:スキル開放ポイント10を消費"),
+        s"${GRAY}スキルポイントコスト:600"))
     setItem(1, 6, Material.MAGENTA_GLAZED_TERRACOTTA, effect = false, s"${GREEN}メニューに戻る", List(s"${GRAY}メニューに戻ります。"))
     registerMotion(motion)
     open()
@@ -88,7 +113,13 @@ class SelectSkillMenu(ryoServerAssist: RyoServerAssist) extends Menu {
       case 10 => toggle.effect(PotionEffectType.NIGHT_VISION, 0, 600, SkillNames(7))
       case 11 => toggle.effect(PotionEffectType.FIRE_RESISTANCE, 0, 600, SkillNames(8))
       case 12 => toggle.effect(PotionEffectType.WATER_BREATHING, 0, 600, SkillNames(9))
-      case 45 => new RyoServerMenu1(ryoServerAssist).menu(p)
+      case 18 => toggle.effect(PotionEffectType.DAMAGE_RESISTANCE, 1, 600, SkillNames(10))
+      case 19 => toggle.effect(PotionEffectType.SPEED, 1, 600, SkillNames(11))
+      case 20 => toggle.effect(PotionEffectType.JUMP, 1, 600, SkillNames(12))
+      case 21 => toggle.effect(PotionEffectType.INCREASE_DAMAGE, 1, 600, SkillNames(13))
+      case 22 => toggle.effect(PotionEffectType.FAST_DIGGING, 1, 600, SkillNames(14))
+      case 23 => toggle.effect(PotionEffectType.REGENERATION, 1, 600, SkillNames(15))
+      case 32 => new RyoServerMenu1(ryoServerAssist).menu(p)
       case 52 =>
         toggle.allEffectClear(p)
         p.sendMessage(AQUA + "スキルをすべて無効化しました。")
