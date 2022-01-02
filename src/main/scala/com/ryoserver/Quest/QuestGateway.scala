1a88445b20caa6887a4da569fe8a10702454550b
rito528 <rotoyutoriapp@gmail.com>
2022-01-02 15:52:57 +0900

    [Add]NeoStackに入っているアイテムで足りるクエストだけを取得する関数の追加

--- src/main/scala/com/ryoserver/Quest/QuestGateway.scala
+++ src/main/scala/com/ryoserver/Quest/QuestGateway.scala
@@ -1,10 +1,14 @@
 package com.ryoserver.Quest
 
 import com.ryoserver.Level.Player.UpdateLevel
+import com.ryoserver.NeoStack.NeoStackGateway
+import com.ryoserver.Player.Data
 import com.ryoserver.Quest.LoadQuests.loadedQuests
 import com.ryoserver.Quest.PlayerQuestData.playerQuestData
 import com.ryoserver.RyoServerAssist
+import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 class QuestGateway {
 
@@ -30,6 +34,15 @@ class QuestGateway {
 
   def getCanQuests(lv: Int): List[QuestType] = {
     LoadQuests.loadedQuests.filter(data => data.minLevel <= lv && data.maxLevel >= lv)
+  }
+
+  def nowNeoStackCanQuest(p:Player,ryoServerAssist: RyoServerAssist): Unit = {
+    val neoStackGateway = new NeoStackGateway(ryoServerAssist)
+    getCanQuests(Data.playerData(p.getUniqueId).level)
+      .filter(_.questType == "delivery") //neoStackからできるクエストのソートということはすべて納品クエストのはず
+      .filter(data => data.requireList
+        .forall{requires => neoStackGateway.getNeoStackAmount(p,new ItemStack(Material.matchMaterial(requires._1))) >= requires._2}
+      )
   }
 
   def questClear(p: Player, ryoServerAssist: RyoServerAssist): Unit = {
