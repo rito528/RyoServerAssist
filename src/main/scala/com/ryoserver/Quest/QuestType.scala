9ac050d6d75883ec7931923beec879e38956891c
rito528 <rotoyutoriapp@gmail.com>
2022-01-02 16:13:08 +0900

    [Add]クエストソート用のデータ型を宣言

--- src/main/scala/com/ryoserver/Quest/QuestType.scala
+++ src/main/scala/com/ryoserver/Quest/QuestType.scala
@@ -1,5 +1,7 @@
 package com.ryoserver.Quest
 
+import enumeratum._
+
 /*
   このcaseClassはクエストのデータ用の型です。
   第1引数にはMaterialNameもしくはEntityTypeNameを入れてください。
@@ -12,3 +14,18 @@ case class QuestType(questName: String,
                      exp: Double,
                      requireList: Map[String, Int],
                     )
+
+/*
+  このobjectはクエスト表示順序を宣言するためのobjectです。
+ */
+
+sealed trait questSortType extends EnumEntry
+
+object questSortType extends Enum[questSortType] {
+
+  case object normal extends questSortType
+  case object neoStack extends questSortType
+  case object bookMark extends questSortType
+
+  override def values: IndexedSeq[questSortType] = findValues
+}
