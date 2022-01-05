bd87ff953d8dd33774d2635ccb23d10b5d6e54ed
rito528 <rotoyutoriapp@gmail.com>
2022-01-05 17:53:17 +0900

    [Add]setPlayerData拡張クラスの作成

--- src/main/scala/com/ryoserver/Player/PlayerManager.scala
+++ src/main/scala/com/ryoserver/Player/PlayerManager.scala
@@ -25,4 +25,21 @@ object PlayerManager {
     def isAutoStack: Boolean  = playerData.autoStack
   }
 
+  implicit class setPlayerData(p:Player) {
+    val rp = new RyoServerPlayer(p)
+    def giveNormalGachaTickets(amount: Int) : Unit = rp.giveNormalGachaTicket(amount)
+    def reduceNormalGachaTickets(amount: Int): Unit = rp.reduceNormalGachaTicket(amount)
+    def addOneVoteNumber(): Unit = rp.addOneVoteNumber()
+    def addGachaPullNumber(number: Int): Unit = rp.addGachaPullNumber(number)
+    def questExpAddNaturally(addExp: Double): Unit = rp.addExp(addExp)
+    def toggleAutoStack(): Unit = rp.toggleAutoStack()
+    def addSkillOpenPoint(addPoint: Int): Unit = rp.addSkillOpenPoint(addPoint)
+    def addSpecialSkillOpenPoint(addPoint:Int): Unit = rp.addSpecialSkillOpenPoint(addPoint)
+    def openSkills(openedSkills: String): Unit = rp.skillOpen(openedSkills)
+    def openSpecialSkills(openedSpecialSkills: String): Unit = rp.specialSkillOpen(openedSpecialSkills)
+    def setLastDistributionReceived(id: Int): Unit = rp.setLastDistributionReceived(id)
+    def openTitles(openedTitles: String): Unit = rp.openTitle(openedTitles)
+    def setSelectedTitle(title: String): Unit = rp.setSelectedTitle(title)
+  }
+
 }
