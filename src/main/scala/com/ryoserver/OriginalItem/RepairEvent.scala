92d527f0ee4d45413ac83f688cd09bdb8fc71a0e
rito528 <rotoyutoriapp@gmail.com>
2022-01-08 14:16:30 +0900

    [Fix]砥石においてオリジナルアイテムの検知が正しくできていない不具合を修正

--- src/main/scala/com/ryoserver/OriginalItem/RepairEvent.scala
+++ src/main/scala/com/ryoserver/OriginalItem/RepairEvent.scala
@@ -15,13 +15,14 @@ class RepairEvent extends Listener {
 
   @EventHandler
   def grindStone(e: InventoryClickEvent): Unit = {
-    if (e.getClickedInventory != null && e.getClickedInventory.getType != InventoryType.GRINDSTONE && e.getSlot != 2) return
-    val item1 = e.getInventory.getItem(0)
-    val item2 = e.getInventory.getItem(1)
-    if (item1 == null && item2 == null) return
-    if (item1 != null && (itemList.contains(item1) || metaList.contains(item1.getItemMeta.getItemFlags)) ||
-      item2 != null && (itemList.contains(item2) || metaList.contains(item2.getItemMeta.getItemFlags))) {
-      e.setResult(Event.Result.DENY)
+    if (e.getClickedInventory != null && e.getClickedInventory.getType == InventoryType.GRINDSTONE && e.getSlot == 2) {
+      val item1 = e.getInventory.getItem(0)
+      val item2 = e.getInventory.getItem(1)
+      if (item1 == null && item2 == null) return
+      if (item1 != null && (itemList.contains(item1) || metaList.contains(item1.getItemMeta.getItemFlags)) ||
+        item2 != null && (itemList.contains(item2) || metaList.contains(item2.getItemMeta.getItemFlags))) {
+        e.setResult(Event.Result.DENY)
+      }
     }
   }
 
