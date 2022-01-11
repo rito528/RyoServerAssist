b2c15288a1cb204c23cb5080b8230d4034dcfc8f
rito528 <rotoyutoriapp@gmail.com>
2022-01-11 18:36:02 +0900

    [Fix]スキルポイント回復アイテムを利用した際の処理がまとめられたのでまとめた

--- src/main/scala/com/ryoserver/SkillSystems/SkillPoint/RecoverySkillPointEvent.scala
+++ src/main/scala/com/ryoserver/SkillSystems/SkillPoint/RecoverySkillPointEvent.scala
@@ -11,24 +11,16 @@ class RecoverySkillPointEvent extends Listener {
   def onEat(e: PlayerItemConsumeEvent): Unit = {
     val item = Item.getOneItemStack(e.getItem)
     val p = e.getPlayer
+    val playerLevel = p.getQuestLevel
+    val playerSP = p.getSkillPoint
+    val maxSP = new SkillPointCal().getMaxSkillPoint(playerLevel)
     if (item == RecoveryItems.min) {
-      val skillPointData = new SkillPointData()
-      val playerLevel = p.getQuestLevel
-      val playerSP = p.getSkillPoint
-      val maxSP = new SkillPointCal().getMaxSkillPoint(playerLevel)
       if (playerSP + 300 >= maxSP) p.setSkillPoint(maxSP)
       else p.setSkillPoint(playerSP + 300)
     } else if (item == RecoveryItems.mid) {
-      val skillPointData = new SkillPointData()
-      val playerLevel = p.getQuestLevel
-      val playerSP = p.getSkillPoint
-      val maxSP = new SkillPointCal().getMaxSkillPoint(playerLevel)
       if (playerSP + 3000 >= maxSP) p.setSkillPoint(maxSP)
       else p.setSkillPoint(playerSP + 3000)
     } else if (item == RecoveryItems.max) {
-      val skillPointData = new SkillPointData()
-      val playerLevel = p.getQuestLevel
-      val maxSP = new SkillPointCal().getMaxSkillPoint(playerLevel)
       p.setSkillPoint(maxSP)
     }
   }
