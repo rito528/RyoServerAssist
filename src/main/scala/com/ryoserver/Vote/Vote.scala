437184039bb443b383bc3fcb487b158777c1f4f5
rito528 <rotoyutoriapp@gmail.com>
2022-01-13 18:06:32 +0900

    [Update]updateVoteContinueを追加

--- src/main/scala/com/ryoserver/Vote/Vote.scala
+++ src/main/scala/com/ryoserver/Vote/Vote.scala
@@ -2,8 +2,9 @@ package com.ryoserver.Vote
 
 import com.ryoserver.Config.ConfigData.getConfig
 import com.ryoserver.Player.PlayerManager.setPlayerData
-import com.ryoserver.util.Player
+import com.ryoserver.util.SQL
 import com.vexsoftware.votifier.model.VotifierEvent
+import org.bukkit.entity.Player
 import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.{Bukkit, ChatColor, Sound}
 
@@ -12,7 +13,7 @@ class Vote extends Listener {
   @EventHandler
   def onVotifierEvent(e: VotifierEvent): Unit = {
     val site = if (e.getVote.getServiceName == "minecraft.jp") "JapanMinecraftServers" else "monocraft"
-    val uuid = Player.nameFromUUID(e.getVote.getUsername)
+    val uuid = com.ryoserver.util.Player.nameFromUUID(e.getVote.getUsername)
     Bukkit.getOnlinePlayers.forEach(onlinePlayer => {
       onlinePlayer.playSound(onlinePlayer.getLocation, Sound.BLOCK_NOTE_BLOCK_BELL, 1, 1)
     })
@@ -25,6 +26,20 @@ class Vote extends Listener {
       p.sendMessage("JMS: " + ChatColor.UNDERLINE + getConfig.voteSite.head)
       p.sendMessage("monocraft: " + ChatColor.UNDERLINE + getConfig.voteSite(1))
     })
+  }
+
+  def updateVoteContinue(p: Player): Unit = {
+    val sql = new SQL()
+    val query = "UPDATE Players SET ContinueVoteNumber = CASE WHEN DATEDIFF(LastVote, NOW()) <= -1 THEN ContinueVoteNumber + 1 ELSE ContinueVoteNumber " +
+      "END," +
+      "LastVote = CASE WHEN DATEDIFF(LastVote, NOW()) = -1 THEN ContinueVoteNumber + 1 " +
+      "WHEN DATEDIFF(LastVote, NOW()) <> 0 AND DATEDIFF(LastVote, NOW()) <= -1 THEN 0 " +
+      "ELSE ContinueVoteNumber " +
+      "END," +
+      "LastVote = NOW() " +
+      s"WHERE UUID='${p.getUniqueId.toString}';"
+    sql.executeSQL(query)
+    sql.close()
   }
 
 }
