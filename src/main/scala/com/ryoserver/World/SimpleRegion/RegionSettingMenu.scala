ee08caddcc24b4774ae0a4f898ad3b656351164f
rito528 <rotoyutoriapp@gmail.com>
2022-01-19 14:29:58 +0900

    [Update]ReginSettingMenuを新フレームワークに移行

--- src/main/scala/com/ryoserver/World/SimpleRegion/RegionSettingMenu.scala
+++ src/main/scala/com/ryoserver/World/SimpleRegion/RegionSettingMenu.scala
@@ -1,7 +1,6 @@
 package com.ryoserver.World.SimpleRegion
 
-import com.ryoserver.Menu.Menu
-import com.ryoserver.Menu.MenuLayout.getLayOut
+import com.ryoserver.Menu.{Menu, MenuButton}
 import com.ryoserver.RyoServerAssist
 import com.ryoserver.util.WorldGuardWrapper
 import com.sk89q.worldguard.protection.flags.{Flags, StateFlag}
@@ -26,32 +25,52 @@ class RegionSettingMenu(ryoServerAssist: RyoServerAssist) extends Menu {
       return
     }
     val set = worldGuard.getRegion(loc).head
-    setItem(2, 1, Material.TNT, effect = false, s"$RED${BOLD}保護を削除します。", List(s"$RED${BOLD}取扱注意！", s"$RED${BOLD}保護範囲を削除します。"))
-    setItem(4, 1, Material.OAK_DOOR, effect = false, s"${GREEN}フラグ:useを切り替えます。", List(s"${GRAY}ドアやボタンの使用を許可します。",
+    val region = worldGuard.getRegion(p.getLocation()).head
+    setButton(MenuButton(2, 1, Material.TNT, s"$RED${BOLD}保護を削除します。", List(s"$RED${BOLD}取扱注意！", s"$RED${BOLD}保護範囲を削除します。"))
+    .setLeftClickMotion(deleteRegion))
+
+    setButton(MenuButton(4, 1, Material.OAK_DOOR, s"${GREEN}フラグ:useを切り替えます。", List(s"${GRAY}ドアやボタンの使用を許可します。",
       s"${GRAY}状態:${if (getFlagStatus(set, Flags.USE)) s"${AQUA}許可" else s"${RED}拒否"}"))
-    setItem(6, 1, Material.OAK_BUTTON, effect = false, s"${GREEN}フラグ:interactを切り替えます。",
+      .setLeftClickMotion(worldGuard.toggleFlag(region, Flags.USE, _)))
+
+    setButton(MenuButton(6, 1, Material.OAK_BUTTON, s"${GREEN}フラグ:interactを切り替えます。",
       List(s"${GRAY}スイッチの使用を許可します。",
         s"${GRAY}状態:${if (getFlagStatus(set, Flags.INTERACT)) s"${AQUA}許可" else s"${RED}拒否"}"))
-    setItem(8, 1, Material.CHEST, effect = false, s"${GREEN}フラグ:chest-accessを切り替えます。",
+    .setLeftClickMotion(worldGuard.toggleFlag(region, Flags.INTERACT, _)))
+
+    setButton(MenuButton(8, 1, Material.CHEST, s"${GREEN}フラグ:chest-accessを切り替えます。",
       List(s"${GRAY}チェストの使用を許可します。",
         s"${GRAY}状態:${if (getFlagStatus(set, Flags.CHEST_ACCESS)) s"${AQUA}許可" else s"${RED}拒否"}"))
-    setItem(2, 2, Material.WHITE_BED, effect = false, s"${GREEN}フラグ:sleepを許可します。",
+    .setLeftClickMotion(worldGuard.toggleFlag(region, Flags.CHEST_ACCESS, _)))
+
+    setButton(MenuButton(2, 2, Material.WHITE_BED, s"${GREEN}フラグ:sleepを許可します。",
       List(s"${GRAY}ベットで眠る許可をします。",
         s"${GRAY}状態:${if (getFlagStatus(set, Flags.SLEEP)) s"${AQUA}許可" else s"${RED}拒否"}"))
-    setItem(4, 2, Material.MINECART, effect = false, s"${GREEN}フラグ:vehicle-placeを許可します。",
+    .setLeftClickMotion(worldGuard.toggleFlag(region, Flags.SLEEP, _)))
+
+    setButton(MenuButton(4, 2, Material.MINECART, s"${GREEN}フラグ:vehicle-placeを許可します。",
       List(s"${GRAY}トロッコ、ボードの設置を許可します。",
         s"${GRAY}状態:${if (getFlagStatus(set, Flags.PLACE_VEHICLE)) s"${AQUA}許可" else s"${RED}拒否"}"))
-    setItem(6, 2, Material.OAK_BOAT, effect = false, s"${GREEN}フラグ:vehicle-destroyを許可します。",
+    .setLeftClickMotion(worldGuard.toggleFlag(region, Flags.PLACE_VEHICLE, _)))
+
+    setButton(MenuButton(6, 2, Material.OAK_BOAT, s"${GREEN}フラグ:vehicle-destroyを許可します。",
       List(s"${GRAY}トロッコ、ボードの破壊を許可します。",
         s"${GRAY}状態:${if (getFlagStatus(set, Flags.DESTROY_VEHICLE)) s"${AQUA}許可" else s"${RED}拒否"}"))
-    setItem(8, 2, Material.ENDER_EYE, effect = false, s"${GREEN}保護範囲の2点を確認します。", List(s"${GRAY}クリックでエフェクトを再生します。"))
-    setItem(2, 4, Material.GRASS_BLOCK, effect = false, s"${GREEN}フラグ:block-placeを許可します。",
+    .setLeftClickMotion(worldGuard.toggleFlag(region, Flags.DESTROY_VEHICLE, _)))
+
+    setButton(MenuButton(8, 2, Material.ENDER_EYE, s"${GREEN}保護範囲の2点を確認します。", List(s"${GRAY}クリックでエフェクトを再生します。"))
+    .setLeftClickMotion(checkRegion))
+
+    setButton(MenuButton(2, 4, Material.GRASS_BLOCK, s"${GREEN}フラグ:block-placeを許可します。",
       List(s"${GRAY}ブロックの設置を許可します。",
-      s"${GRAY}状態:${if (getFlagStatus(set, Flags.BLOCK_PLACE)) s"${AQUA}許可" else s"${RED}拒否"}"))
-    setItem(4, 4, Material.STONE_PICKAXE, effect = false, s"${GREEN}フラグ:block-breakを許可します。",
+        s"${GRAY}状態:${if (getFlagStatus(set, Flags.BLOCK_PLACE)) s"${AQUA}許可" else s"${RED}拒否"}"))
+    .setLeftClickMotion(worldGuard.toggleFlag(region, Flags.BLOCK_PLACE, _)))
+
+    setButton(MenuButton(4, 4, Material.STONE_PICKAXE, s"${GREEN}フラグ:block-breakを許可します。",
       List(s"${GRAY}ブロックの破壊を許可します。",
         s"${GRAY}状態:${if (getFlagStatus(set, Flags.BLOCK_BREAK)) s"${AQUA}許可" else s"${RED}拒否"}"))
-    registerMotion(motion)
+    .setLeftClickMotion(worldGuard.toggleFlag(region, Flags.BLOCK_BREAK, _)))
+    build()
     open()
   }
 
@@ -81,43 +100,6 @@ class RegionSettingMenu(ryoServerAssist: RyoServerAssist) extends Menu {
         }
       }
     }.runTaskTimerAsynchronously(ryoServerAssist, 10, 10)
-  }
-
-  def motion(p: Player, index: Int): Unit = {
-    val worldGuard = new WorldGuardWrapper
-    val region = worldGuard.getRegion(p.getLocation()).head
-    val motions = Map[Int, Player => Unit](
-      getLayOut(2, 1) -> deleteRegion,
-      getLayOut(4, 1) -> {
-        worldGuard.toggleFlag(region, Flags.USE, _)
-      },
-      getLayOut(6, 1) -> {
-        worldGuard.toggleFlag(region, Flags.INTERACT, _)
-      },
-      getLayOut(8, 1) -> {
-        worldGuard.toggleFlag(region, Flags.CHEST_ACCESS, _)
-      },
-      getLayOut(2, 2) -> {
-        worldGuard.toggleFlag(region, Flags.SLEEP, _)
-      },
-      getLayOut(4, 2) -> {
-        worldGuard.toggleFlag(region, Flags.PLACE_VEHICLE, _)
-      },
-      getLayOut(6, 2) -> {
-        worldGuard.toggleFlag(region, Flags.DESTROY_VEHICLE, _)
-      },
-      getLayOut(8, 2) -> checkRegion,
-      getLayOut(2, 4) -> {
-        worldGuard.toggleFlag(region, Flags.BLOCK_PLACE, _)
-      },
-      getLayOut(4, 4) -> {
-        worldGuard.toggleFlag(region, Flags.BLOCK_BREAK, _)
-      }
-    )
-    if (motions.contains(index)) {
-      motions(index)(p)
-      openMenu(p)
-    }
   }
 
   def getFlagStatus(set: ProtectedRegion, flag: StateFlag): Boolean = {
