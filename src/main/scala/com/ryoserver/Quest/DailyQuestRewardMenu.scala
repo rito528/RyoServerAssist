66119330bf136ba5a2caef6a2d51136bdab270e1
rito528 <rotoyutoriapp@gmail.com>
2022-01-20 06:03:26 +0900

    [Update]DailyQuestRewardMenuを新フレームワークに移行

--- src/main/scala/com/ryoserver/Quest/DailyQuestRewardMenu.scala
+++ src/main/scala/com/ryoserver/Quest/DailyQuestRewardMenu.scala
@@ -1,6 +1,6 @@
 package com.ryoserver.Quest
 
-import com.ryoserver.Menu.Menu
+import com.ryoserver.Menu.{Menu, MenuButton}
 import com.ryoserver.Menu.MenuLayout.getLayOut
 import com.ryoserver.Player.PlayerManager.setPlayerData
 import com.ryoserver.RyoServerAssist
@@ -16,22 +16,29 @@ class DailyQuestRewardMenu(ryoServerAssist: RyoServerAssist) extends Menu {
 
   def openRewardMenu(player: Player): Unit = {
     p = player
-    setItem(2,1,Material.PAPER,effect = true,s"${GREEN}ガチャ券を受け取る",List(s"${GRAY}報酬としてガチャ券を16枚受け取ります。"))
-    setItem(4,1,Material.EXPERIENCE_BOTTLE,effect = true,s"${GREEN}今回のクエストの経験値1.2倍",List(s"${GRAY}報酬として今回のクエストの経験値を1.2倍にします。"))
-    registerMotion(motion)
+    setButton(MenuButton(2,1,Material.PAPER,s"${GREEN}ガチャ券を受け取る",List(s"${GRAY}報酬としてガチャ券を16枚受け取ります。"))
+    .setLeftClickMotion(rewardGachaTicket)
+    .setEffect())
+    setButton(MenuButton(4,1,Material.EXPERIENCE_BOTTLE,s"${GREEN}今回のクエストの経験値1.2倍",List(s"${GRAY}報酬として今回のクエストの経験値を1.2倍にします。"))
+    .setLeftClickMotion(rewardQuestExp)
+    .setEffect())
+    build(new DailyQuestRewardMenu(ryoServerAssist).openRewardMenu)
     open()
   }
 
-  def motion(p:Player,index: Int): Unit = {
+  private def rewardGachaTicket(p: Player): Unit = {
     val questGateway = new QuestGateway
-    if (index == getLayOut(2,1)) {
-      questGateway.dailyQuestClear(p, ryoServerAssist,1.0)
-      p.giveNormalGachaTickets(16)
-      p.sendMessage(s"${AQUA}デイリークエストの報酬として、ガチャ券を16枚配布しました。")
-    } else if (index == getLayOut(4,1)) {
-      questGateway.dailyQuestClear(p, ryoServerAssist,1.2)
-      p.sendMessage(s"${AQUA}デイリークエストの報酬として、今回のクエストの経験値を1.2倍にしました。")
-    }
+    questGateway.dailyQuestClear(p, ryoServerAssist,1.0)
+    p.giveNormalGachaTickets(16)
+    p.sendMessage(s"${AQUA}デイリークエストの報酬として、ガチャ券を16枚配布しました。")
+    p.playSound(p.getLocation, Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1)
+    p.closeInventory()
+  }
+
+  private def rewardQuestExp(p: Player): Unit = {
+    val questGateway = new QuestGateway
+    questGateway.dailyQuestClear(p, ryoServerAssist,1.2)
+    p.sendMessage(s"${AQUA}デイリークエストの報酬として、今回のクエストの経験値を1.2倍にしました。")
     p.playSound(p.getLocation, Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1)
     p.closeInventory()
   }
