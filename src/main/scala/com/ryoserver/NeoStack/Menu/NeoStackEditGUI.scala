6360ba4dd0eb2437ee95577f09f8e1dbc40f8bbb
rito528 <rotoyutoriapp@gmail.com>
2022-01-22 01:01:06 +0900

    [Update]NeoStackEditGUIを新フレームワークに更新

--- src/main/scala/com/ryoserver/NeoStack/Menu/NeoStackEditGUI.scala
+++ src/main/scala/com/ryoserver/NeoStack/Menu/NeoStackEditGUI.scala
@@ -1,7 +1,7 @@
 package com.ryoserver.NeoStack.Menu
 
 import com.ryoserver.Menu.MenuLayout.{getX, getY}
-import com.ryoserver.Menu.{Menu, RyoServerMenu1}
+import com.ryoserver.Menu.{Menu, MenuButton, RyoServerMenu1}
 import com.ryoserver.NeoStack.PlayerCategory.getSelectedCategory
 import com.ryoserver.NeoStack.{ItemList, LoadNeoStackPage, NeoStackGateway}
 import com.ryoserver.RyoServerAssist
@@ -34,45 +34,50 @@ class NeoStackEditGUI(ryoServerAssist: RyoServerAssist) extends Menu {
       }
       index += 1
     })
-    setItem(1, 6, Material.MAGENTA_GLAZED_TERRACOTTA, effect = false, s"${GREEN}前のページに戻ります。", List(s"${GRAY}クリックで戻ります。"))
-    if (p.hasPermission("ryoserverassist.neoStack")) {
-      setItem(5, 6, Material.NETHER_STAR, effect = false, "クリックでリストを保存します。", List("カテゴリ:" + getSelectedCategory(p) + "のリストを保存します。"))
-    }
-    setItem(9, 6, Material.MAGENTA_GLAZED_TERRACOTTA, effect = false, s"${GREEN}次のページに移動します。", List(s"${GRAY}クリックで移動します。"))
+    setButton(MenuButton(1, 6, Material.MAGENTA_GLAZED_TERRACOTTA, s"${GREEN}前のページに戻ります。", List(s"${GRAY}クリックで戻ります。"))
+    .setLeftClickMotion(backMenu))
+    setButton(MenuButton(5, 6, Material.NETHER_STAR, "クリックでリストを保存します。", List("カテゴリ:" + getSelectedCategory(p) + "のリストを保存します。"))
+    .setLeftClickMotion(save))
+    setButton(MenuButton(9, 6, Material.MAGENTA_GLAZED_TERRACOTTA, s"${GREEN}次のページに移動します。", List(s"${GRAY}クリックで移動します。"))
+    .setLeftClickMotion(nextPage))
     partButton = true
     buttons :+= 45
     buttons :+= 49
     buttons :+= 53
-    registerMotion(motion)
+    build(new NeoStackEditGUI(ryoServerAssist).openAddGUI(_,1,null))
     open()
   }
 
-  def motion(p: Player, index: Int): Unit = {
-    val data = new NeoStackGateway()
+  private def backMenu(p: Player): Unit = {
     val nowPage = p.getOpenInventory.getTitle.replace("neoStackアイテム追加メニュー:", "").toInt
-    if (index == 49) {
-      var invIndex = 0
-      var invItem = ""
-      p.getOpenInventory.getTopInventory.getContents.foreach(is => {
-        if (invIndex < 45) {
-          invItem += Item.getStringFromItemStack(is) + ";"
-        }
-        invIndex += 1
-      })
-      data.editItemList(getSelectedCategory(p), nowPage, invItem)
-      p.sendMessage(s"${AQUA}カテゴリリスト:${getSelectedCategory(p)}を編集しました。")
-      new LoadNeoStackPage(ryoServerAssist).loadStackPage()
-      ItemList.stackList = mutable.Map.empty
-      ItemList.loadItemList(ryoServerAssist)
-    } else if (index == 45) {
-      if (nowPage != 1) {
-        new NeoStackEditGUI(ryoServerAssist).openAddGUI(p, nowPage - 1, getSelectedCategory(p))
-      } else if (nowPage == 1) {
-        new RyoServerMenu1(ryoServerAssist).menu(p)
-      }
-    } else if (index == 53) {
-      new NeoStackEditGUI(ryoServerAssist).openAddGUI(p, nowPage + 1, getSelectedCategory(p))
+    if (nowPage != 1) {
+      new NeoStackEditGUI(ryoServerAssist).openAddGUI(p, nowPage - 1, getSelectedCategory(p))
+    } else if (nowPage == 1) {
+      new RyoServerMenu1(ryoServerAssist).menu(p)
     }
+  }
+
+  private def save(p: Player): Unit = {
+    val nowPage = p.getOpenInventory.getTitle.replace("neoStackアイテム追加メニュー:", "").toInt
+    val data = new NeoStackGateway()
+    var invIndex = 0
+    var invItem = ""
+    p.getOpenInventory.getTopInventory.getContents.foreach(is => {
+      if (invIndex < 45) {
+        invItem += Item.getStringFromItemStack(is) + ";"
+      }
+      invIndex += 1
+    })
+    data.editItemList(getSelectedCategory(p), nowPage, invItem)
+    p.sendMessage(s"${AQUA}カテゴリリスト:${getSelectedCategory(p)}を編集しました。")
+    new LoadNeoStackPage(ryoServerAssist).loadStackPage()
+    ItemList.stackList = mutable.Map.empty
+    ItemList.loadItemList(ryoServerAssist)
+  }
+
+  private def nextPage(p: Player): Unit = {
+    val nowPage = p.getOpenInventory.getTitle.replace("neoStackアイテム追加メニュー:", "").toInt
+    new NeoStackEditGUI(ryoServerAssist).openAddGUI(p, nowPage + 1, getSelectedCategory(p))
   }
 
 }
