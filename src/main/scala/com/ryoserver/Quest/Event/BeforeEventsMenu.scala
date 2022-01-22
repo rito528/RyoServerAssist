fd60a230b4fb3a86339b4ab4bd81c7a474502f0c
rito528 <rotoyutoriapp@gmail.com>
2022-01-22 14:01:01 +0900

    [Update]BeforeEventsMenuを新フレームワークに移行

--- src/main/scala/com/ryoserver/Quest/Event/BeforeEventsMenu.scala
+++ src/main/scala/com/ryoserver/Quest/Event/BeforeEventsMenu.scala
@@ -1,7 +1,7 @@
 package com.ryoserver.Quest.Event
 
 import com.ryoserver.Menu.MenuLayout.{getLayOut, getX, getY}
-import com.ryoserver.Menu.{Menu, RyoServerMenu1}
+import com.ryoserver.Menu.{Menu, MenuButton, RyoServerMenu1}
 import com.ryoserver.RyoServerAssist
 import org.bukkit.ChatColor._
 import org.bukkit.Material
@@ -19,34 +19,39 @@ class BeforeEventsMenu(ryoServerAssist: RyoServerAssist) extends Menu {
     var invIndex = 0
     EventDataProvider.oldEventData.zipWithIndex.foreach { case ((eventName, playerData), index) =>
       if (index < (getLayOut(9, 5) + 1) * page && (getLayOut(9, 5) + 1) * (page - 1) <= index) {
-        setItem(getX(invIndex), getY(invIndex), Material.BOOK, effect = false, s"$WHITE$eventName", List(
+        setButton(MenuButton(getX(invIndex), getY(invIndex), Material.BOOK, s"$WHITE$eventName", List(
           s"${WHITE}あなたの順位:${if (playerData.contains(p.getUniqueId)) playerData.toSeq.sortBy(_._2).reverse.toMap.keys.toList.indexOf(p.getUniqueId) else "参加していません。"}",
           s"${WHITE}貢献数:${if (playerData.contains(p.getUniqueId)) playerData(p.getUniqueId) else "参加していません。"}"
-        ))
+        )))
         invIndex += 1
       }
     }
     if (page == 1) {
-      setItem(1, 6, Material.MAGENTA_GLAZED_TERRACOTTA, effect = false, s"${GREEN}メニューに戻ります。", List(s"${GRAY}クリックで戻ります。"))
+      setButton(MenuButton(1, 6, Material.MAGENTA_GLAZED_TERRACOTTA, s"${GREEN}メニューに戻ります。", List(s"${GRAY}クリックで戻ります。"))
+      .setLeftClickMotion(backPage))
     } else {
-      setItem(1, 6, Material.MAGENTA_GLAZED_TERRACOTTA, effect = false, s"${GREEN}前のページに戻ります。", List(s"${GRAY}クリックで戻ります。"))
+      setButton(MenuButton(1, 6, Material.MAGENTA_GLAZED_TERRACOTTA, s"${GREEN}前のページに戻ります。", List(s"${GRAY}クリックで戻ります。"))
+      .setLeftClickMotion(backPage))
     }
-    setItem(9, 6, Material.MAGENTA_GLAZED_TERRACOTTA, effect = false, s"${GREEN}次のページに移動します。", List(s"${GRAY}クリックで移動します。"))
-    registerMotion(motion)
+    setButton(MenuButton(9, 6, Material.MAGENTA_GLAZED_TERRACOTTA, s"${GREEN}次のページに移動します。", List(s"${GRAY}クリックで移動します。"))
+    .setLeftClickMotion(nextPage))
+    build(new BeforeEventsMenu(ryoServerAssist).openMenu(_,1))
     open()
   }
 
-  private def motion(p: Player, index: Int): Unit = {
+  private def backPage(p: Player): Unit = {
     val page = p.getOpenInventory.getTitle.replace("過去のイベント:", "").toInt
-    if (getLayOut(1, 6) == index) {
-      if (page == 1) {
-        new RyoServerMenu1(ryoServerAssist).menu(p)
-      } else {
-        new BeforeEventsMenu(ryoServerAssist).openMenu(p, page - 1)
-      }
-    } else if (getLayOut(9, 6) == index) {
-      new BeforeEventsMenu(ryoServerAssist).openMenu(p, page + 1)
+    if (page == 1) {
+      new RyoServerMenu1(ryoServerAssist).menu(p)
+    } else {
+      new BeforeEventsMenu(ryoServerAssist).openMenu(p, page - 1)
     }
   }
+
+  private def nextPage(p: Player): Unit = {
+    val page = p.getOpenInventory.getTitle.replace("過去のイベント:", "").toInt
+    new BeforeEventsMenu(ryoServerAssist).openMenu(p, page + 1)
+  }
+
 
 }
