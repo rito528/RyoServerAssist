e45d574e29f667f46512fc9b8e70202cf324f614
rito528 <rotoyutoriapp@gmail.com>
2022-01-22 18:17:02 +0900

    [Fix]Expをフォーマットする際にthreeCommaFormat関数を利用するように

--- src/main/scala/com/ryoserver/Level/Player/BossBar.scala
+++ src/main/scala/com/ryoserver/Level/Player/BossBar.scala
@@ -1,6 +1,7 @@
 package com.ryoserver.Level.Player
 
 import com.ryoserver.Level.CalLv
+import com.ryoserver.util.Format
 import org.bukkit.Bukkit
 import org.bukkit.boss.{BarColor, BarStyle}
 import org.bukkit.entity.Player
@@ -13,11 +14,11 @@ object BossBar {
 
   def createLevelBer(exp: Double, p: Player): Unit = {
     val calLv = new CalLv
-    val formattedExp = f"${exp.toInt}%,3d" + f"${exp - exp.toInt}%.1f".replace("0.",".")
+    val formattedExp = Format.threeCommaFormat(exp)
     val bossBer = Bukkit.createBossBar("Lv." + calLv.getLevel(exp.toInt) + " 総EXP:" + formattedExp , BarColor.GREEN, BarStyle.SOLID)
     val lv = calLv.getLevel(exp.toInt)
     val remainingExp = calLv.getSumTotal(lv + 1) - exp
-    val formattedRemainingExp = f"${remainingExp.toInt}%,3d" + f"${remainingExp - remainingExp.toInt}%.1f".replace("0.",".")
+    val formattedRemainingExp = Format.threeCommaFormat(remainingExp)
     if (calLv.MAX_LV > lv) {
       bossBer.setTitle("Lv." + calLv.getLevel(exp.toInt) + " 総EXP:" + formattedExp + " 次のレベルまで残り: " + formattedRemainingExp)
       bossBer.setProgress(1.0 - ((calLv.getSumTotal(lv + 1) - exp) / calLv.getExp(lv + 1)))
@@ -33,9 +34,9 @@ object BossBar {
     val calLv = new CalLv
     val lv = calLv.getLevel(exp.toInt)
     val bossBer = bers.get(p)
-    val formattedExp = f"${exp.toInt}%,3d" + f"${exp - exp.toInt}%.1f".replace("0.",".")
+    val formattedExp = Format.threeCommaFormat(exp)
     val remainingExp = calLv.getSumTotal(lv + 1) - exp
-    val formattedRemainingExp = f"${remainingExp.toInt}%,3d" + f"${remainingExp - remainingExp.toInt}%.1f".replace("0.",".")
+    val formattedRemainingExp = Format.threeCommaFormat(remainingExp)
     if (calLv.MAX_LV > lv) {
       bossBer.get.setTitle("Lv." + calLv.getLevel(exp.toInt) + " 総EXP:" + formattedExp + " 次のレベルまで残り: " + formattedRemainingExp)
       bossBer.get.setProgress(1.0 - ((calLv.getSumTotal(lv + 1) - exp) / calLv.getExp(lv + 1)))
