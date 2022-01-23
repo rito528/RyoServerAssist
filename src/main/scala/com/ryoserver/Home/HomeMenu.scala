720024924e902dee46a8c9cc3568c8c890ef7460
rito528 <rotoyutoriapp@gmail.com>
2022-01-24 06:07:42 +0900

    [Fix]indexがおかしいので修正

--- src/main/scala/com/ryoserver/Home/HomeMenu.scala
+++ src/main/scala/com/ryoserver/Home/HomeMenu.scala
@@ -7,16 +7,17 @@ import org.bukkit.ChatColor._
 
 class HomeMenu extends Menu {
 
-  override val slot: Int = 6
+  override val slot: Int = 3
   override var name: String = "ホームメニュー"
   override var p: Player = _
 
   def openHomeMenu(player: Player): Unit = {
     p = player
     for (x <- 3 to 7 by 2) {
-      setButton(MenuButton(x,1,Material.WHITE_BED,s"$RESET${WHITE}ホーム${x}を設定します。",List(s"${GRAY}クリックでホーム${x}を設定します。")))
-      setButton(MenuButton(x,2,Material.COMPASS,s"${WHITE}ホーム${x}にテレポートします。",List(s"${GRAY}テレポート地点: ")))
-      setButton(MenuButton(x,3,Material.BLUE_WOOL,s"$RESET${WHITE}ホーム${x}をロックします。",List(s"${GRAY}クリックでホーム${x}をロックします。")))
+      val index = (x - 1) / 2
+      setButton(MenuButton(x,1,Material.WHITE_BED,s"$RESET${WHITE}ホーム${index}を設定します。",List(s"${GRAY}クリックでホーム${index}を設定します。")))
+      setButton(MenuButton(x,2,Material.COMPASS,s"${WHITE}ホーム${index}にテレポートします。",List(s"${GRAY}テレポート地点: ")))
+      setButton(MenuButton(x,3,Material.BLUE_WOOL,s"$RESET${WHITE}ホーム${index}をロックします。",List(s"${GRAY}クリックでホーム${index}をロックします。")))
     }
     build(new HomeMenu().openHomeMenu)
     open()
