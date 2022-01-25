5096ed111226156907e697f3ae2021b603e60793
rito528 <rotoyutoriapp@gmail.com>
2022-01-25 19:12:23 +0900

    [Add]setHomePointを追加

--- src/main/scala/com/ryoserver/Home/HomeGateway.scala
+++ src/main/scala/com/ryoserver/Home/HomeGateway.scala
@@ -1,5 +1,19 @@
 package com.ryoserver.Home
 
-class HomeGateway {
+import org.bukkit.Location
+
+import java.util.UUID
+
+class HomeGateway(uuid: UUID) {
+
+  def setHomePoint(point: Int,location: Location): Unit = {
+    HomeData.removeHomeData(uuid,point)
+    HomeData.addHomeData(HomeDataType(
+      UUID = uuid,
+      point = point,
+      location = location,
+      isLocked = false
+    ))
+  }
 
 }
