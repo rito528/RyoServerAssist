51e4f7d4821267529271729ba1d2adaf6d22d161
rito528 <rotoyutoriapp@gmail.com>
2022-01-28 22:29:50 +0900

    [Add]isBetweenTime関数を追加

---
+++ src/main/scala/com/ryoserver/util/Calender.scala
@@ -0,0 +1,16 @@
+package com.ryoserver.util
+
+import java.text.SimpleDateFormat
+import java.util.{Calendar, TimeZone}
+
+object Calender {
+
+  def isBetweenTime(start: String,end: String): Boolean = {
+    val regex = "[0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}".r
+    require(!regex.matches(start) || !regex.matches(end),"時間の指定フォーマットが違います。yyyy/MM/dd HH:mm")
+    val nowCalender = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"))
+    val format = new SimpleDateFormat("yyyy/MM/dd HH:mm")
+    nowCalender.getTime.after(format.parse(start)) && nowCalender.getTime.before(format.parse(end))
+  }
+
+}
