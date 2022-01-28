65527a82b86f160bb2570c08a599fa58fdb6011d
rito528 <rotoyutoriapp@gmail.com>
2022-01-28 23:19:53 +0900

    [Fix]レベルのアップデート処理を最適化

--- src/main/scala/com/ryoserver/util/Calendar.scala
+++ src/main/scala/com/ryoserver/util/Calendar.scala
@@ -1,14 +1,14 @@
 package com.ryoserver.util
 
 import java.text.SimpleDateFormat
-import java.util.{Calendar, TimeZone}
+import java.util.TimeZone
 
-object Calender {
+object Calendar {
 
   def isBetweenTime(start: String,end: String): Boolean = {
     val regex = "[0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}".r
     require(!regex.matches(start) || !regex.matches(end),"時間の指定フォーマットが違います。yyyy/MM/dd HH:mm")
-    val nowCalender = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"))
+    val nowCalender = java.util.Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"))
     val format = new SimpleDateFormat("yyyy/MM/dd HH:mm")
     nowCalender.getTime.after(format.parse(start)) && nowCalender.getTime.before(format.parse(end))
   }
