529ec05efc4040af03deec04b9730970c4c02407
rito528 <rotoyutoriapp@gmail.com>
2022-02-03 23:52:42 +0900

    [Update]updateReLoginをScalikeJDBCに移行

--- src/main/scala/com/ryoserver/Player/UpdateData.scala
+++ src/main/scala/com/ryoserver/Player/UpdateData.scala
@@ -2,21 +2,20 @@ package com.ryoserver.Player
 
 import com.ryoserver.util.SQL
 import org.bukkit.entity.Player
+import scalikejdbc.{AutoSession, scalikejdbcSQLInterpolationImplicitDef}
 
 class UpdateData {
 
   def updateReLogin(p: Player): Unit = {
-    val sql = new SQL()
-    val query = "UPDATE Players SET loginDays = CASE WHEN DATEDIFF(lastLogin, NOW()) <= -1 THEN loginDays + 1 ELSE loginDays " +
-      "END," +
-      "consecutiveLoginDays = CASE WHEN DATEDIFF(lastLogin, NOW()) = -1 THEN consecutiveLoginDays + 1 " +
-      "WHEN DATEDIFF(lastLogin, NOW()) <> 0 AND DATEDIFF(lastLogin, NOW()) <= -1 THEN 0 " +
-      "ELSE consecutiveLoginDays " +
-      "END," +
-      "lastLogin = NOW() " +
-      s"WHERE UUID='${p.getUniqueId.toString}';"
-    sql.executeSQL(query)
-    sql.close()
+    implicit val session: AutoSession.type = AutoSession
+    sql"""UPDATE Players SET loginDays = CASE WHEN DATEDIFF(lastLogin, NOW()) <= -1 THEN loginDays + 1 ELSE loginDays
+      END,
+      consecutiveLoginDays = CASE WHEN DATEDIFF(lastLogin, NOW()) = -1 THEN consecutiveLoginDays + 1
+      WHEN DATEDIFF(lastLogin, NOW()) <> 0 AND DATEDIFF(lastLogin, NOW()) <= -1 THEN 0
+      ELSE consecutiveLoginDays
+      END,
+      lastLogin = NOW()
+      WHERE UUID=${p.getUniqueId.toString}""".execute.apply()
   }
 
 }
