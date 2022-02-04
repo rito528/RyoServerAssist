b340e43ee1bae101c3803cbd5493e072fc3bbd6e
rito528 <rotoyutoriapp@gmail.com>
2022-02-05 00:07:57 +0900

    [Update]neoStackPageloadをScalikeJDBCに移行

--- src/main/scala/com/ryoserver/NeoStack/LoadNeoStackPage.scala
+++ src/main/scala/com/ryoserver/NeoStack/LoadNeoStackPage.scala
@@ -3,7 +3,10 @@ package com.ryoserver.NeoStack
 import com.ryoserver.NeoStack.ItemList.itemList
 import com.ryoserver.NeoStack.NeoStackPageData.stackPageData
 import com.ryoserver.RyoServerAssist
+import com.ryoserver.util.ScalikeJDBC.getData
 import com.ryoserver.util.{Item, SQL}
+import org.bukkit.Bukkit
+import scalikejdbc.{AutoSession, scalikejdbcSQLInterpolationImplicitDef}
 
 import scala.collection.mutable
 
@@ -11,25 +14,24 @@ class LoadNeoStackPage(implicit ryoServerAssist: RyoServerAssist) {
 
   def loadStackPage(): Unit = {
     ryoServerAssist.getLogger.info("neoStackページをロード中...")
-    val sql = new SQL()
-    val rs = sql.executeQuery(s"SELECT * FROM StackList")
+    implicit val session: AutoSession.type = AutoSession
+    val stackListTable = sql"SELECT * FROM StackList"
     stackPageData = mutable.Map.empty
-    while (rs.next()) {
-      val category = rs.getString("category")
-      val invItems = rs.getString("invItem")
-      val page = rs.getInt("page")
-      if (!stackPageData.contains(category)) {
-        stackPageData += (category -> mutable.Map(page -> invItems))
+    if (stackListTable.getHeadData.nonEmpty) {
+      case class StackData(category: String, invItems: String,page: Int)
+      val stackData = stackListTable.map(rs => {
+        StackData(rs.string("category"),rs.string("invItem"),rs.int("page"))
+      }).headOption.first.apply().get
+      if (!stackPageData.contains(stackData.category)) {
+        stackPageData += (stackData.category -> mutable.Map(stackData.page -> stackData.invItems))
       } else {
-        stackPageData(category) += (page -> invItems)
+        stackPageData(stackData.category) += (stackData.page -> stackData.invItems)
       }
-      invItems.split(";").foreach(itemStack => {
-        if (Item.getItemStackFromString(itemStack) != null) {
-          itemList += Item.getOneItemStack(Item.getItemStackFromString(itemStack))
-        }
+      stackData.invItems.split(";").foreach(itemStackString => {
+        val itemStack = Item.getItemStackFromString(itemStackString)
+        if (itemStack != null) itemList += itemStack
       })
     }
-    sql.close()
     ryoServerAssist.getLogger.info("neoStackページのロードが完了しました。")
   }
 
