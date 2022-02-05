3bb2c505799da2a8d3e853902c8002a8b3b3928b
rito528 <rotoyutoriapp@gmail.com>
2022-02-05 15:40:01 +0900

    [Update]StorageのloadをScalikeJDBCに移行

--- src/main/scala/com/ryoserver/Storage/Storage.scala
+++ src/main/scala/com/ryoserver/Storage/Storage.scala
@@ -1,7 +1,7 @@
 package com.ryoserver.Storage
 
 import com.ryoserver.Player.PlayerManager.getPlayerData
-import com.ryoserver.util.SQL
+import com.ryoserver.util.{Item, SQL}
 import com.ryoserver.util.ScalikeJDBC.getData
 import org.bukkit.ChatColor._
 import org.bukkit.configuration.file.YamlConfiguration
@@ -26,22 +26,19 @@ class Storage {
 
   def load(p: Player): Unit = {
     if (p.getQuestLevel >= 10) {
-      val sql = new SQL()
-      val invData_rs = sql.executeQuery(s"SELECT invData FROM Storage WHERE UUID='${p.getUniqueId.toString}';")
+      implicit val session: AutoSession.type = AutoSession
       val inv = Bukkit.createInventory(null, 54, "Storage")
-      var counter = 0
-      if (invData_rs.next()) {
-        val invData = invData_rs.getString("invData").split(";")
-        val config = new YamlConfiguration
-        invData.foreach(material => {
-          config.loadFromString(material)
-          inv.setItem(counter, config.getItemStack("i", null))
-          counter += 1
+      val storageTable = sql"SELECT invData FROM Storage WHERE UUID=${p.getUniqueId.toString}"
+      if (storageTable.getHeadData.nonEmpty) {
+        storageTable.foreach(rs => {
+          rs.string("invData").split(";").zipWithIndex.foreach{case (itemStackString,index) =>
+            val itemStack = Item.getItemStackFromString(itemStackString)
+            inv.setItem(index,itemStack)
+          }
         })
       }
       p.openInventory(inv)
       p.playSound(p.getLocation, Sound.BLOCK_CHEST_OPEN, 1, 1)
-      sql.close()
     } else {
       p.sendMessage(s"${RED}ストレージ機能はLv.10以上になると使うことができます。")
     }
