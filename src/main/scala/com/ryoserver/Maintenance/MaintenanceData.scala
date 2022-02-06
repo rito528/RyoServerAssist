b6a297dc4d60391d8d428ba1aa7ba8c7966d5e17
rito528 <rotoyutoriapp@gmail.com>
2022-02-06 18:39:21 +0900

    [Update]Maintenanceの状態変更ができるように

--- src/main/scala/com/ryoserver/Maintenance/MaintenanceData.scala
+++ src/main/scala/com/ryoserver/Maintenance/MaintenanceData.scala
@@ -1,7 +1,24 @@
 package com.ryoserver.Maintenance
 
+import scalikejdbc.{AutoSession, scalikejdbcSQLInterpolationImplicitDef}
+
 object MaintenanceData {
 
-  var isMaintenance = false
+  private var isMaintenance = false
+
+  def loadMaintenance(): Unit = {
+    implicit val autoSession: AutoSession.type  = AutoSession
+    sql"SELECT * FROM ServerMaintenance".foreach(rs => {
+      isMaintenance = rs.boolean("isMaintenance")
+    })
+  }
+
+  def setMaintenance(isEnable: Boolean): Unit = {
+    implicit val autoSession: AutoSession.type = AutoSession
+    sql"UPDATE ServerMaintenance SET isMaintenance=$isEnable".execute().apply()
+    isMaintenance = isEnable
+  }
+
+  def getMaintenance: Boolean = isMaintenance
 
 }
