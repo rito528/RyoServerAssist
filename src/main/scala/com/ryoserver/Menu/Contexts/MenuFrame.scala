6293f1a0dc86542bb144702ab9aae1046dd749d3
rito528 <rotoyutoriapp@gmail.com>
2022-02-08 18:40:00 +0900

    [Fix]Frameのタイポを修正

--- src/main/scala/com/ryoserver/Menu/Contexts/MenuFrame.scala
+++ src/main/scala/com/ryoserver/Menu/Contexts/MenuFrame.scala
@@ -4,7 +4,7 @@ import com.ryoserver.Menu.session.MenuSessions
 import org.bukkit.Bukkit.createInventory
 import org.bukkit.inventory.Inventory
 
-case class MenuFlame(row: Int,title: String) {
+case class MenuFrame(row: Int, title: String) {
   require(row <= 6)
 
   def createMenu(): Inventory = createInventory(MenuSessions.session,row,title)
---
+++ src/main/scala/com/ryoserver/Menu/Menu.scala
@@ -0,0 +1,9 @@
+package com.ryoserver.Menu
+
+import com.ryoserver.Menu.Contexts.MenuFrame
+
+trait Menu {
+
+  val frame: MenuFrame
+
+}
