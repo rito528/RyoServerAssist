002bee6f062e866bf3201d43c689a61ae037f948
rito528 <rotoyutoriapp@gmail.com>
2022-02-09 16:13:29 +0900

    [Update]旧メニューハンドラーを停止

--- src/main/scala/com/ryoserver/Menu/MenuHandler.scala
+++ src/main/scala/com/ryoserver/Menu/MenuHandler.scala
@@ -1,10 +1,9 @@
 package com.ryoserver.Menu
 
-import com.ryoserver.Menu.Button.LeftClickMotion
 import com.ryoserver.Menu.session.MenuSession
 import org.bukkit.entity.Player
-import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.{EventHandler, Listener}
 
 class MenuHandler extends Listener {
 
@@ -26,9 +25,9 @@ class MenuHandler extends Listener {
 
     //右クリック、左クリック以外を排除
     if (!e.getClick.isLeftClick && !e.getClick.isRightClick) {
-      e.setCancelled(true)
       return
     }
+    holder.runMotion(e.getSlot,e)
 
   }
 
--- src/main/scala/com/ryoserver/RyoServerAssist.scala
+++ src/main/scala/com/ryoserver/RyoServerAssist.scala
@@ -12,7 +12,7 @@ import com.ryoserver.File.CreateFiles
 import com.ryoserver.Gacha.{Gacha, GachaItemChangeGUI, GachaLoader}
 import com.ryoserver.Home.HomeData
 import com.ryoserver.Maintenance.MaintenanceData
-import com.ryoserver.Menu.MenuHandlerOld
+import com.ryoserver.Menu.{MenuHandler, MenuHandlerOld}
 import com.ryoserver.NeoStack.Menu.SelectStackMenu
 import com.ryoserver.NeoStack._
 import com.ryoserver.Notification.Notification
@@ -119,7 +119,8 @@ class RyoServerAssist extends JavaPlugin {
       new GachaItemChangeGUI,
       new Vote,
       new SecurityEvent,
-      new MenuHandlerOld,
+//      new MenuHandlerOld,
+      new MenuHandler,
       new EventDeliveryMenu,
       new PlayEffect,
       new EditEvent,
