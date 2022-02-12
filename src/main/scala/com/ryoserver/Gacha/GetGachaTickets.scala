ec6d578f94009076967b6abf905f5cc926f21316
rito528 <rotoyutoriapp@gmail.com>
2022-02-12 21:18:38 +0900

    [Update]通常ガチャ券を受け取る際に直接インベントリに配られるように

--- src/main/scala/com/ryoserver/Gacha/GetGachaTickets.scala
+++ src/main/scala/com/ryoserver/Gacha/GetGachaTickets.scala
@@ -1,6 +1,7 @@
 package com.ryoserver.Gacha
 
 import com.ryoserver.Player.PlayerManager.{getPlayerData, setPlayerData}
+import com.ryoserver.util.Item
 import org.bukkit.ChatColor._
 import org.bukkit.Sound
 import org.bukkit.entity.Player
@@ -11,8 +12,13 @@ class GetGachaTickets() {
     val gachaTicket = GachaPaperData.normal
     val number = getTickets(p)
     if (number != 0) {
+      val oldAmount = p.getInventory.getContents
+        .filter(itemStack =>
+          (if (itemStack != null) Item.getOneItemStack(itemStack) else null) == Item.getOneItemStack(gachaTicket)).map(_.getAmount).sum
       gachaTicket.setAmount(number)
-      p.getWorld.dropItemNaturally(p.getLocation(), gachaTicket)
+      p.reduceNormalGachaTickets(p.getInventory.getContents
+        .filter(itemStack =>
+          (if (itemStack != null) Item.getOneItemStack(itemStack) else null) == Item.getOneItemStack(gachaTicket)).map(_.getAmount).sum - oldAmount)
       p.sendMessage(s"${AQUA}ガチャ券を受け取りました。")
       p.playSound(p.getLocation, Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1)
     } else {
@@ -23,10 +29,8 @@ class GetGachaTickets() {
   def getTickets(p: Player): Int = {
     val number = p.getGachaTickets
     if (number >= 576) {
-      p.reduceNormalGachaTickets(576)
       576
     } else {
-      p.reduceNormalGachaTickets(number)
       number
     }
   }
