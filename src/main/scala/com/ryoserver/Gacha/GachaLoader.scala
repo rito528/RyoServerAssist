345e710f854cbe35721437fce0c201bc7ce72e92
rito528 <rotoyutoriapp@gmail.com>
2022-02-13 10:52:39 +0900

    [Fix]ガチャアイテムのロード時の処理を変更

--- src/main/scala/com/ryoserver/Gacha/GachaLoader.scala
+++ src/main/scala/com/ryoserver/Gacha/GachaLoader.scala
@@ -8,74 +8,36 @@ import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import scalikejdbc.{AutoSession, scalikejdbcSQLInterpolationImplicitDef}
 
+import scala.collection.mutable
+
 object GachaLoader {
 
+  private implicit val session: AutoSession.type = AutoSession
+
   // 1 = miss, 2 = per,3 = bigPer, 4 = special
-  var perItemList: Array[ItemStack] = Array.empty
-  var bigPerItemList: Array[ItemStack] = Array.empty
-  var specialItemList: Array[ItemStack] = Array.empty
-  var missItemList: Array[ItemStack] = Array.empty
+  lazy val perItemList: mutable.Set[ItemStack] = getGachaItems(1)
+  lazy val bigPerItemList: mutable.Set[ItemStack] = getGachaItems(2)
+  lazy val specialItemList: mutable.Set[ItemStack] = getGachaItems(3)
+  lazy val missItemList: mutable.Set[ItemStack] = getGachaItems(4)
 
-  var per: Double = _ //あたり
-  var bigPer: Double = _ //大当たり
-  var special: Double = _ //特等
+  private def getGachaItems(rarity: Int): mutable.Set[ItemStack] = {
+    mutable.Set() ++ sql"SELECT * FROM GachaItems WHERE Rarity=$rarity"
+      .map(rs => Item.getOneItemStack(Item.getItemStackFromString(rs.string("Material")))).toList().apply()
+  }
 
+  lazy val per: Double = getConfig.per //あたり
+  lazy val bigPer: Double = getConfig.bigPer //大当たり
+  lazy val special: Double = getConfig.Special //特等
+  lazy val miss: Double = 100.0 - per - bigPer - special //ハズレ
+
+  @deprecated //このクラスはあくまでloaderなのでaddItemがあるのはおかしい
   def addGachaItem(implicit ryoServerAssist: RyoServerAssist, is: ItemStack, rarity: Int): Unit = {
-    implicit val session: AutoSession.type = AutoSession
     sql"INSERT INTO GachaItems(Rarity,Material) VALUES ($rarity,${Item.getStringFromItemStack(Item.getOneItemStack(is))})"
       .execute.apply()
-    unload(ryoServerAssist)
-    load(ryoServerAssist)
   }
 
-  def load(implicit ryoServerAssist: RyoServerAssist): Unit = {
-    gachaItemLoad(ryoServerAssist)
-    gachaRarityLoad(ryoServerAssist)
-  }
-
-  private def gachaItemLoad(implicit ryoServerAssist: RyoServerAssist): Unit = {
-    ryoServerAssist.getLogger.info("ガチャアイテムロード中....")
-    implicit val session: AutoSession.type = AutoSession
-    val gachaItemTable = sql"SELECT * FROM GachaItems"
-    gachaItemTable.foreach(rs => {
-      val rarity = rs.int("Rarity")
-      val itemStack = Item.getOneItemStack(Item.getItemStackFromString(rs.string("Material")))
-      if (rarity == 0) missItemList :+= itemStack
-      else if (rarity == 1) perItemList :+= itemStack
-      else if (rarity == 2) bigPerItemList :+= itemStack
-      else if (rarity == 3) specialItemList :+= itemStack
-    })
-    ryoServerAssist.getLogger.info("ガチャアイテムロードが完了しました！")
-  }
-
-  private def gachaRarityLoad(implicit ryoServerAssist: RyoServerAssist): Unit = {
-    /*
-      各レアリティの割合を計算
-     */
-    ryoServerAssist.getLogger.info("ガチャ排出割合読み込み中...")
-    per = getConfig.per
-    bigPer = getConfig.bigPer
-    special = getConfig.Special
-    val miss = 100.0 - per - bigPer - special
-    if (miss <= 0) {
-      ryoServerAssist.getLogger.warning("ガチャ割合の指定が不正です。")
-      ryoServerAssist.getLogger.warning("サーバーを停止します。")
-      Bukkit.shutdown()
-    }
-    ryoServerAssist.getLogger.info("ガチャ排出割合読み込みが完了しました！")
-  }
-
-  def unload(implicit ryoServerAssist: RyoServerAssist): Unit = {
-    ryoServerAssist.getLogger.info("ガチャリストをアンロードしています...")
-    perItemList = Array.empty
-    bigPerItemList = Array.empty
-    specialItemList = Array.empty
-    missItemList = Array.empty
-    ryoServerAssist.getLogger.info("ガチャリストをアンロードしました。")
-  }
-
+  @deprecated
   def listGachaItem(rarity: Int, p: Player): Unit = {
-    implicit val session: AutoSession.type = AutoSession
     p.sendMessage("ガチャアイテムリスト")
     p.sendMessage("+--------------------------+")
     sql"SELECT * FROM GachaItems WHERE Rarity=$rarity".foreach(rs => {
@@ -88,8 +50,8 @@ object GachaLoader {
     p.sendMessage("+--------------------------+")
   }
 
+  @deprecated
   def removeGachaItem(id: Int): Unit = {
-    implicit val session: AutoSession.type = AutoSession
     sql"DELETE FROM GachaItems WHERE id=$id".execute.apply()
   }
 }
