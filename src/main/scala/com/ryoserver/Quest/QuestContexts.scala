d51252cf70844854b72dca1e9419d1b24f1209ca
rito528 <rotoyutoriapp@gmail.com>
2022-02-14 11:41:32 +0900

    [Update]クエストのロード処理を再実装

--- src/main/scala/com/ryoserver/Quest/QuestContexts.scala
+++ src/main/scala/com/ryoserver/Quest/QuestContexts.scala
@@ -1,7 +1,9 @@
 package com.ryoserver.Quest
 
+import enumeratum.{Enum, EnumEntry}
+
 case class QuestDataContext(questName: String,
-                             questType: String,
+                             questType: QuestType,
                              minLevel: Int,
                              maxLevel: Int,
                              exp: Double,
@@ -11,4 +13,33 @@ case class PlayerQuestDataContext(selectedQuestName: Option[String],
                                   progress: Map[String, Int],
                                   bookmarks: List[String])
 
+/*
+  クエストタイプの型
+ */
+sealed abstract class QuestType(val name: String,val dataName: String) extends EnumEntry
+
+object QuestType extends Enum[QuestType] {
+
+  override def values: IndexedSeq[QuestType] = findValues
+
+  case object delivery extends QuestType("納品","delivery")
+
+  case object suppression extends QuestType("討伐","suppression")
+}
+
+/*
+  クエストのソートタイプ
+ */
+sealed abstract class QuestSortContext(val name: String) extends EnumEntry
+
+object QuestSortContext extends Enum[QuestSortContext] {
+
+  override def values: IndexedSeq[QuestSortContext] = findValues
+
+  case object normal extends QuestSortContext("通常")
+
+  case object neoStack extends QuestSortContext("ネオスタックにあるアイテム")
+
+  case object bookMark extends QuestSortContext("ブックマーク")
+}
 
---
+++ src/main/scala/com/ryoserver/Quest/QuestData.scala
@@ -0,0 +1,45 @@
+package com.ryoserver.Quest
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ryoserver.util.Entity
+import org.bukkit.{Bukkit, Material}
+
+import java.io.File
+import scala.io.Source
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+
+object QuestData {
+
+  private def getQuestData(path: String): Set[QuestDataContext] = {
+    new File(path).listFiles()
+      .map(_.getName)
+      .filter(_.contains(".json"))
+      .toList
+      .map {fileName =>
+        val objectMapper = new ObjectMapper()
+        val source = Source.fromFile(s"$path$fileName","UTF-8")
+        val jsonData = objectMapper.readTree(source.getLines().mkString(""))
+        val questName = jsonData.get("questName").textValue()
+        val questType = QuestType.values.filter(_.dataName == jsonData.get("type").textValue()).head
+        val minLevel = jsonData.get("minLevel").intValue()
+        val maxLevel = jsonData.get("maxLevel").intValue()
+        val exp = jsonData.get("exp").doubleValue()
+        val condition = jsonData.findValuesAsText("condition").asScala
+          .map{data =>
+            val splitData = data.split(":")
+            if (questType == QuestType.delivery && Material.matchMaterial(splitData.head) == null) {
+              Bukkit.shutdown()
+            } else if (questType == QuestType.suppression && !Entity.isExistsEntity(splitData.head)) {
+              Bukkit.shutdown()
+            }
+            splitData.head -> splitData.last.toInt
+          }.toMap
+        QuestDataContext(questName,questType,minLevel,maxLevel,exp,condition)
+      }.toSet
+  }
+
+  final val loadedQuestData: Set[QuestDataContext] = getQuestData("plugins/RyoServerAssist/Quests/")
+
+  final val loadedDailyQuestData: Set[QuestDataContext] = getQuestData("plugins/RyoServerAssist/DailyQuests/")
+
+}
