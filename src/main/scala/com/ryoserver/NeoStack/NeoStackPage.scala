1600a196ba911eada86eef8a8eab760cde7ca8ad
rito528 <rotoyutoriapp@gmail.com>
2022-03-10 07:10:08 +0900

    [Update]NeoStackPageを宣言

--- src/main/scala/com/ryoserver/NeoStack/NeoStackPage.scala
+++ src/main/scala/com/ryoserver/NeoStack/NeoStackPage.scala
@@ -1,6 +1,11 @@
 package com.ryoserver.NeoStack
 
+import org.bukkit.inventory.ItemStack
+
+import scala.collection.mutable
+
 object NeoStackPage {
 
+  val pageData: mutable.Map[RawNeoStackPageContext,List[ItemStack]] = mutable.Map.empty
 
 }
