9669f2869ff7ff20dd553369218e70be054ca6a4
rito528 <rotoyutoriapp@gmail.com>
2022-03-16 17:58:29 +0900

    [Remove]PlayerDataLoaderを削除

--- src/main/scala/com/ryoserver/Player/PlayerEvents.scala
+++ src/main/scala/com/ryoserver/Player/PlayerEvents.scala
@@ -1,7 +1,12 @@
 package com.ryoserver.Player
 
+import com.ryoserver.Level.Player.{BossBar, LevelLoader}
 import com.ryoserver.Maintenance.MaintenanceData.getMaintenance
+import com.ryoserver.NeoStack.NeoStackItem.NeoStackItemRepository
 import com.ryoserver.RyoServerAssist
+import com.ryoserver.SkillSystems.Skill.EffectSkill.SkillOperation
+import com.ryoserver.SkillSystems.Skill.SpecialSkillPlayerData
+import com.ryoserver.SkillSystems.SkillPoint.SkillPointBer
 import com.ryoserver.Title.GiveTitle
 import org.bukkit.event.player.{PlayerJoinEvent, PlayerQuitEvent}
 import org.bukkit.event.{EventHandler, Listener}
@@ -15,7 +20,11 @@ class PlayerEvents(implicit ryoServerAssist: RyoServerAssist) extends Listener {
     if (getMaintenance && !p.hasPermission("ryoserverassist.maintenance")) {
       p.kickPlayer("現在メンテナンス中です。\n\n詳細は公式Twitter、Discordを御覧ください。")
     }
-    new PlayerDataLoader().load(p)
+    new UpdateData().updateReLogin(p)
+    new LevelLoader().loadPlayerLevel(p)
+    new NeoStackItemRepository().restore(p.getUniqueId)
+    SkillPointBer.create(p)
+    new Name().updateName(p)
     val title = new GiveTitle()
     title.continuousLogin(p)
     title.loginDays(p)
@@ -27,7 +36,11 @@ class PlayerEvents(implicit ryoServerAssist: RyoServerAssist) extends Listener {
 
   @EventHandler
   def onQuit(e: PlayerQuitEvent): Unit = {
-    new PlayerDataLoader().unload(e.getPlayer)
+    val p = e.getPlayer
+    BossBar.unloadLevelBer(p)
+    SkillPointBer.remove(p)
+    new SkillOperation(ryoServerAssist).allDisablingSkills(p)
+    if (SpecialSkillPlayerData.getActivatedSkill(p).isDefined) SpecialSkillPlayerData.skillInvalidation(p, SpecialSkillPlayerData.getActivatedSkill(p).get)
     implicit val session: AutoSession.type = AutoSession
     sql"UPDATE Players SET lastLogout=NOW() WHERE UUID=${e.getPlayer.getUniqueId.toString}".execute.apply()
   }
