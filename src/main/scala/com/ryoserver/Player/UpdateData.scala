287bdda3bb63aa0bd7d7d2364e85fbde27adbaf9
rito528 <rotoyutoriapp@gmail.com>
2022-03-20 19:32:43 +0900

    [Fix]同日にログインしても連続ログイン日数が加算されてしまう不具合を修正

--- src/main/scala/com/ryoserver/Player/UpdateData.scala
+++ src/main/scala/com/ryoserver/Player/UpdateData.scala
@@ -13,6 +13,7 @@ class UpdateData {
     val nowDay = DateUtils.truncate(new Date,Calendar.DAY_OF_MONTH).getTime
     val oneWeekMilliSec = 604800000 //1週間をミリ秒で表す
     val ryoServerData = p.getRyoServerData
+    if (nowDay - lastVoteDay == 0) return
     if (nowDay - lastVoteDay <= oneWeekMilliSec) { //1週間以内だったら
       ryoServerData.setConsecutiveLoginDays(ryoServerData.consecutiveLoginDays + 1).apply
     } else {
